7c4b510020ae75d7c6f5432127c6a6b49311a174
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-01-08 11:47:42 +0000

    Add Untar helper to untar downloaded files

--- cli/helpers/helpers_suite_test.go
+++ cli/helpers/helpers_suite_test.go
@@ -1,6 +1,8 @@
 package helpers_test
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -10,4 +12,14 @@ import (
 func TestHelpers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Helpers Suite")
+}
+
+// FixturePath returns the full path to a file in "fixtures" directory
+func FixturePath(fileName string) string {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return path.Join(currentPath, "..", "fixtures", fileName)
 }
