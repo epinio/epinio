ca71d59b5584666f134d10dbc4286b13ca2a6d3a
Andreas Kupries <andreas.kupries@suse.com>
2021-01-18 09:20:27 +0100

    Removed call of removed function, missed in merge.

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -37,8 +37,6 @@ func Execute() {
 	rootCmd.AddCommand(client.CmdCreateOrg)
 	rootCmd.AddCommand(client.CmdPush)
 
-	ExitIfError(ensureKubeConfig())
-
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
