a96747c2ba4b4e0ff6fc26e574d1f4c2af7f2adf
Andreas Kupries <andreas.kupries@suse.com>
2021-01-29 12:00:02 +0100

    [feat] Added the basics needed to support logging of cli internals.
    Based on `go-logr/logr` for API, and using the `go-logr/stdr` implementation of that API.
    Can switch to different implementations if needed.

---
+++ cli/kubernetes/config/logger.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/stdr"
+)
+
+// Verbosity returns the verbosity argument
+func Verbosity() int {
+	return viper.GetInt("verbosity")
+}
+
+// LoggerFlags adds to viper flags
+func LoggerFlags(pf *flag.FlagSet, argToEnv map[string]string) {
+	pf.IntP("verbosity", "", 0, "Only print log messages at or above this level")
+	viper.BindPFlag("verbosity", pf.Lookup("verbosity"))
+	argToEnv["verbosity"] = "VERBOSITY"
+}
+
+// New creates a new logger with our setup
+func NewClientLogger() logr.Logger {
+	stdr.SetVerbosity(Verbosity())
+	return NewLogger().WithName("CarrierClient")
+}
+
+// New creates a new logger with our setup
+func NewInstallClientLogger() logr.Logger {
+	stdr.SetVerbosity(Verbosity())
+	return NewLogger().WithName("InstallClient")
+}
+
+// New creates a new logger with our setup
+func NewLogger() logr.Logger {
+	stdr.SetVerbosity(Verbosity())
+	return stdr.New(nil).V(1)
+}
