a92c06bd022e713c1a65feaf1acd285708b0372d
Tim Hardeck <thardeck@suse.com>
2021-02-11 14:13:56 +0100

    Improve orgs acceptance test

--- acceptance/orgs_test.go
+++ acceptance/orgs_test.go
@@ -13,19 +13,16 @@ var _ = Describe("Orgs", func() {
 	})
 
 	Describe("create-org", func() {
-		BeforeEach(func() {
+		It("creates and targets an org", func() {
+			By("creating an org")
 			_, err := Carrier("create-org mycreatedorg", "")
 			Expect(err).ToNot(HaveOccurred())
-		})
-
-		It("creates an org", func() {
 			orgs, err := Carrier("orgs", "")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(orgs).To(MatchRegexp("mycreatedorg"))
-		})
 
-		It("targets an org", func() {
-			_, err := Carrier("target mycreatedorg", "")
+			By("targeting an org")
+			_, err = Carrier("target mycreatedorg", "")
 			Expect(err).ToNot(HaveOccurred())
 			out, err := Carrier("target", "")
 			Expect(err).ToNot(HaveOccurred())
