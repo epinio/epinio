200a50bb380742f9c0626b773678b7cf236cc6e9
Andreas Kupries <andreas.kupries@suse.com>
2021-02-11 11:27:57 +0100

    feat: Modified `push` command to accept an optional path argument. fix:  Updated README to take this into account.
    The path to the application sources.
    Default, for when not specified, stays at the current working directory.
    Readme now has nicer example.
    
    TODO: Have `push` block with timeout for user to abort a bad attempt.

--- cmd/internal/client/push.go
+++ cmd/internal/client/push.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"os"
+
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/suse/carrier/cli/paas"
@@ -10,9 +12,9 @@ var ()
 
 // CmdPush implements the carrier orgs command
 var CmdPush = &cobra.Command{
-	Use:   "push NAME",
+	Use:   "push NAME [PATH_TO_APPLICATION_SOURCES]",
 	Short: "Push an application from the current working directory",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.RangeArgs(1, 2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client, cleanup, err := paas.NewCarrierClient(cmd.Flags(), nil)
 		defer func() {
@@ -25,8 +27,17 @@ var CmdPush = &cobra.Command{
 			return errors.Wrap(err, "error initializing cli")
 		}
 
-		// TODO - add a parameter for path
-		err = client.Push(args[0], ".")
+		var path string
+		if len(args) == 1 {
+			path, err = os.Getwd()
+			if err != nil {
+				return errors.Wrap(err, "error pushing app")
+			}
+		} else {
+			path = args[1]
+		}
+
+		err = client.Push(args[0], path)
 		if err != nil {
 			return errors.Wrap(err, "error pushing app")
 		}
