a2c8fc2e5e45bab84b92b15f393441354f88bb83
Tim Hardeck <thardeck@suse.com>
2021-02-12 13:36:03 +0100

    Add acceptance tests (#135)
    * Add acceptance test suite with basic tests
    * Use these tests in github actions ci
    * Switch `make tests` target to only run unit tests
    
    Co-authored-by: Dimitris Karakasilis <DKarakasilis@suse.com>

---
+++ acceptance/acceptance_suite_test.go
@@ -0,0 +1,208 @@
+package acceptance_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"regexp"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/codeskyblue/kexec"
+	"github.com/onsi/ginkgo/config"
+	"github.com/suse/carrier/cli/helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestAcceptance(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Acceptance Suite")
+}
+
+var nodeSuffix, nodeTmpDir string
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	if os.Getenv("REGISTRY_USERNAME") == "" || os.Getenv("REGISTRY_PASSWORD") == "" {
+		panic("REGISTRY_USERNAME and REGISTRY_PASSWORD environment variables can't be empty")
+	}
+
+	fmt.Printf("Compiling Carrier on node %d\n", config.GinkgoConfig.ParallelNode)
+
+	buildCarrier()
+	return []byte(strconv.Itoa(int(time.Now().Unix())))
+}, func(randomSuffix []byte) {
+	var err error
+
+	nodeSuffix = fmt.Sprintf("%d-%s",
+		config.GinkgoConfig.ParallelNode, string(randomSuffix))
+	nodeTmpDir, err = ioutil.TempDir("", "carrier-"+nodeSuffix)
+	if err != nil {
+		panic("Could not create temp dir: " + err.Error())
+	}
+
+	copyCarrier()
+	fmt.Printf("Creating a cluster for node %d\n", config.GinkgoConfig.ParallelNode)
+	createCluster()
+	os.Setenv("KUBECONFIG", nodeTmpDir+"/kubeconfig")
+
+	fmt.Printf("Creating image pull secret for Dockerhub on node %d\n", config.GinkgoConfig.ParallelNode)
+	helpers.Kubectl(fmt.Sprintf("create secret docker-registry regcred --docker-server=%s --docker-username=%s --docker-password=%s",
+		"https://index.docker.io/v1/",
+		os.Getenv("REGISTRY_USERNAME"),
+		os.Getenv("REGISTRY_PASSWORD"),
+	))
+
+	fmt.Printf("Installing Carrier on node %d\n", config.GinkgoConfig.ParallelNode)
+	installCarrier()
+
+	// Allow things to settle. Shouldn't be needed after we fix this:
+	// https://github.com/SUSE/carrier/issues/108
+	fmt.Printf("Waiting for things to settle on node %d\n", config.GinkgoConfig.ParallelNode)
+
+	WaitForInstallationToSettle(180)
+	fmt.Printf("Done waiting on node %d\n", config.GinkgoConfig.ParallelNode)
+})
+
+var _ = AfterSuite(func() {
+	fmt.Printf("Uninstall carrier on node %d\n", config.GinkgoConfig.ParallelNode)
+	out, _ := uninstallCarrier()
+	match, _ := regexp.MatchString(`Carrier uninstalled`, out)
+	if !match {
+		panic("Uninstalling carrier failed: " + out)
+	}
+	fmt.Printf("Deleting cluster on node %d\n", config.GinkgoConfig.ParallelNode)
+	deleteCluster()
+	fmt.Printf("Deleting tmpdir on node %d\n", config.GinkgoConfig.ParallelNode)
+	deleteTmpDir()
+})
+
+func createCluster() {
+	name := fmt.Sprintf("carrier-acceptance-%s", nodeSuffix)
+
+	if _, err := exec.LookPath("k3d"); err != nil {
+		panic("Couldn't find k3d in PATH: " + err.Error())
+	}
+
+	_, err := RunProc("k3d cluster create "+name, nodeTmpDir, false)
+	if err != nil {
+		panic("Creating k3d cluster failed: " + err.Error())
+	}
+
+	kubeconfig, err := RunProc("k3d kubeconfig get "+name, nodeTmpDir, false)
+	if err != nil {
+		panic("Getting kubeconfig failed: " + err.Error())
+	}
+	err = ioutil.WriteFile(path.Join(nodeTmpDir, "kubeconfig"), []byte(kubeconfig), 0644)
+	if err != nil {
+		panic("Writing kubeconfig failed: " + err.Error())
+	}
+}
+
+func deleteCluster() {
+	name := fmt.Sprintf("carrier-acceptance-%s", nodeSuffix)
+
+	if _, err := exec.LookPath("k3d"); err != nil {
+		panic("Couldn't find k3d in PATH: " + err.Error())
+	}
+
+	output, err := RunProc("k3d cluster delete "+name, nodeTmpDir, false)
+	if err != nil {
+		panic(fmt.Sprintf("Deleting k3d cluster failed: %s\n %s\n",
+			output, err.Error()))
+	}
+}
+
+func deleteTmpDir() {
+	err := os.RemoveAll(nodeTmpDir)
+	if err != nil {
+		panic(fmt.Sprintf("Failed deleting temp dir %s: %s\n",
+			nodeTmpDir, err.Error()))
+	}
+}
+
+func RunProc(cmd, dir string, toStdout bool) (string, error) {
+	p := kexec.CommandString(cmd)
+
+	var b bytes.Buffer
+	if toStdout {
+		p.Stdout = io.MultiWriter(os.Stdout, &b)
+		p.Stderr = io.MultiWriter(os.Stderr, &b)
+	} else {
+		p.Stdout = &b
+		p.Stderr = &b
+	}
+
+	p.Dir = dir
+
+	if err := p.Run(); err != nil {
+		return b.String(), err
+	}
+
+	err := p.Wait()
+	return b.String(), err
+}
+
+func buildCarrier() {
+	output, err := RunProc("make", "..", false)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't build Carrier: %s\n %s\n"+err.Error(), output))
+	}
+}
+
+func copyCarrier() {
+	output, err := RunProc("cp dist/carrier-* "+nodeTmpDir+"/carrier", "..", false)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't copy Carrier: %s\n %s\n"+err.Error(), output))
+	}
+}
+
+func installCarrier() (string, error) {
+	return Carrier("install", "")
+}
+
+func uninstallCarrier() (string, error) {
+	return Carrier("uninstall", "")
+}
+
+// Carrier invokes the `carrier` binary, running the specified command.
+// It returns the command output and/or error.
+// dir parameter defines the directory from which the command should be run.
+// It defaults to the current dir if left empty.
+func Carrier(command string, dir string) (string, error) {
+	var commandDir string
+	var err error
+
+	if dir == "" {
+		commandDir, err = os.Getwd()
+		if err != nil {
+			return "", err
+		}
+	} else {
+		commandDir = dir
+	}
+
+	cmd := fmt.Sprintf(nodeTmpDir+"/carrier %s", command)
+
+	return RunProc(cmd, commandDir, false)
+}
+
+// Allow things to settle. Shouldn't be needed after we fix this:
+// https://github.com/SUSE/carrier/issues/108
+func WaitForInstallationToSettle(seconds int) {
+	for i := 0; i < seconds; i++ {
+		out, _ := RunProc(`kubectl get pods --namespace tekton-pipelines`, ".", false)
+		match, _ := regexp.MatchString(`tekton-dashboard-[0-9a-z-]+ +1/1`, out)
+		if match {
+			break
+		}
+
+		time.Sleep(time.Second)
+	}
+}
---
+++ acceptance/orgs_test.go
@@ -0,0 +1,36 @@
+package acceptance_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Orgs", func() {
+	It("has a default org", func() {
+		orgs, err := Carrier("orgs", "")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(orgs).To(MatchRegexp("workspace"))
+	})
+
+	Describe("create-org", func() {
+		It("creates and targets an org", func() {
+			By("creating an org")
+			_, err := Carrier("create-org mycreatedorg", "")
+			Expect(err).ToNot(HaveOccurred())
+			orgs, err := Carrier("orgs", "")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(orgs).To(MatchRegexp("mycreatedorg"))
+
+			By("targeting an org")
+			_, err = Carrier("target mycreatedorg", "")
+			Expect(err).ToNot(HaveOccurred())
+			out, err := Carrier("target", "")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(MatchRegexp("Currently targeted organization: mycreatedorg"))
+
+			By("switching org back to default")
+			_, err = Carrier("target workspace", "")
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
