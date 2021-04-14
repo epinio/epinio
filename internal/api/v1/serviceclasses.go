856fcbafaeb4391ad2bec27d77dd70aba7bc69f0
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2021-04-14 14:24:59 +0200

    fix: separate serviceplans into a new controller

--- internal/api/v1/serviceclasses.go
+++ internal/api/v1/serviceclasses.go
@@ -46,8 +46,5 @@ func (scc ServiceClassesController) Index(w http.ResponseWriter, r *http.Request
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(js)
-	if handleError(w, err, http.StatusInternalServerError) {
-		return
-	}
+	w.Write(js)
 }
