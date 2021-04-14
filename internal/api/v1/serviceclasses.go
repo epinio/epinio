0a1a206200280b4000b8361b91a1648e3b316ec3
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2021-04-14 12:26:06 +0200

    feat: add services and serviceclasses controller

---
+++ internal/api/v1/serviceclasses.go
@@ -0,0 +1,96 @@
+package v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/epinio/epinio/internal/cli/clients"
+	"github.com/epinio/epinio/internal/services"
+	"github.com/epinio/epinio/kubernetes"
+	"github.com/julienschmidt/httprouter"
+)
+
+type ServiceClassesController struct {
+}
+
+func (scc ServiceClassesController) Index(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	org := params.ByName("org")
+
+	cluster, err := kubernetes.GetCluster()
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	gitea, err := clients.GetGiteaClient()
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+
+	exists, err := gitea.OrgExists(org)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	if !exists {
+		http.Error(w, fmt.Sprintf("Organization '%s' does not exist", org),
+			http.StatusNotFound)
+		return
+	}
+	serviceClasses, err := services.ListClasses(cluster)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+
+	js, err := json.Marshal(serviceClasses)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (scc ServiceClassesController) ServicePlanList(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	org := params.ByName("org")
+	serviceClassName := params.ByName("serviceclass")
+
+	cluster, err := kubernetes.GetCluster()
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	gitea, err := clients.GetGiteaClient()
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+
+	exists, err := gitea.OrgExists(org)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	if !exists {
+		http.Error(w, fmt.Sprintf("Organization '%s' does not exist", org),
+			http.StatusNotFound)
+		return
+	}
+
+	serviceClass, err := services.ClassLookup(cluster, serviceClassName)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	if serviceClass == nil {
+		http.Error(w, fmt.Sprintf("ServiceClass '%s' does not exist", serviceClassName),
+			http.StatusNotFound)
+		return
+	}
+	servicePlans, err := serviceClass.ListPlans()
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+
+	js, err := json.Marshal(servicePlans)
+	if handleError(w, err, http.StatusInternalServerError) {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
