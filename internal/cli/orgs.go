4d310d356457c7c1e88450ea8292ae2c936576de
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-04-26 10:41:39 +0000

    Move org command in the orgs.go file for consistency with the other cmds

--- internal/cli/orgs.go
+++ internal/cli/orgs.go
@@ -44,3 +44,25 @@ var CmdOrgList = &cobra.Command{
 	SilenceErrors: true,
 	SilenceUsage:  true,
 }
+
+// CmdOrgCreate implements the epinio `orgs create` command
+var CmdOrgCreate = &cobra.Command{
+	Use:   "create NAME",
+	Short: "Creates an organization",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := clients.NewEpinioClient(cmd.Flags())
+		if err != nil {
+			return errors.Wrap(err, "error initializing cli")
+		}
+
+		err = client.CreateOrg(args[0])
+		if err != nil {
+			return errors.Wrap(err, "error creating org")
+		}
+
+		return nil
+	},
+	SilenceErrors: true,
+	SilenceUsage:  true,
+}
