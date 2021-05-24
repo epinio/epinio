3d45a965af747a548100c18b81ed36ea9bbc78a5
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-05-24 10:32:13 +0300

    Allow installation of some of the tools and fix Dockerfile
    Now `make build` requires helm because it is packaging the registry helm
    chart.

--- scripts/tools-install.sh
+++ scripts/tools-install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 source scripts/include/setup.sh
 
+# If arguments are passed to this script, those should be the tools to install.
+# If no arguments are passed, all tools are installed.
+if [ "$#" -gt 0 ]; then
+  TOOLS=("$@")
+fi
+
 # Make sure we have an exact version match for *all* defined tools.
 PINNED_TOOLS=true require_tools "${TOOLS[@]}"
