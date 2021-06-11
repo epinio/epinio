35b82e81eb464489031304fed5c3fda37fb351c1
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2021-06-11 10:41:58 +0200

    Increment k3d version to 4.4.4
    Signed-off-by: Tim Hardeck <thardeck@suse.com>

--- scripts/tools/k3d.sh
+++ scripts/tools/k3d.sh
@@ -3,13 +3,13 @@
 
 TOOLS+=(k3d)
 
-K3D_VERSION="4.1.1"
+K3D_VERSION="4.4.4"
 
 function k3d_version { k3d version; }
 
-K3D_SHA256_DARWIN="40fe273f567bad7e031636fe896a05637f173f047ee41f9566a6ad933e7da34f"
-K3D_SHA256_LINUX="4148b8035774a2765969898cc73066491ac718d1b2890415a4625931aaacd3ac"
-K3D_SHA256_WINDOWS="dc13acba412b473ab5b602420bf61cb2149a8b99cc24a0261574e3f965f03560"
+K3D_SHA256_DARWIN="5fc9b68c9cd523ea743a9ca31163330db642d50f4db421ee00f7e1f4a29da552"
+K3D_SHA256_LINUX="6d4ac3d4c5b084f445980e427c5d3a75eefd2c39a22d028825c234c6c20d1e46"
+K3D_SHA256_WINDOWS="1e09e00ee830247ededc3c4718e62f89c1ec7a634fcdf9defb76d124e6a43b83"
 
 K3D_URL_DARWIN="https://github.com/rancher/k3d/releases/download/v{version}/k3d-darwin-amd64"
 K3D_URL_LINUX="https://github.com/rancher/k3d/releases/download/v{version}/k3d-linux-amd64"
