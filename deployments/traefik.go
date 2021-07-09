20693ca8230bed62d3fa60bdbc10f5ee1f4aff08
Klaus Kämpf <kkaempf@suse.de>
2021-07-09 10:43:28 +0200

    Provide helpful error message if Traefik times out waiting for IP
    Signed-off-by: Klaus Kämpf <kkaempf@suse.de>
    
    [skip ci]

--- deployments/traefik.go
+++ deployments/traefik.go
@@ -135,7 +135,10 @@ func (k Traefik) apply(ctx context.Context, c *kubernetes.Cluster, ui *termui.UI
 	}
 
 	if err := c.WaitUntilServiceHasLoadBalancer(ctx, ui, TraefikDeploymentID, "traefik", duration.ToServiceLoadBalancer()); err != nil {
-		return errors.Wrap(err, "failed waiting for Traefik Ingress to contact the Load Balancer")
+		return errors.Wrap(err, "failed waiting for Traefik Ingress to contact the Load Balancer.\n" +
+			"Ensure your kubernetes platform has the ability to provision LoadBalancer IP address.\n\n" +
+			"Follow these steps to enable this ability\n" +
+			"https://github.com/epinio/epinio/blob/main/docs/install.md")
 	}
 
 	ui.Success().Msg("Traefik Ingress deployed")
