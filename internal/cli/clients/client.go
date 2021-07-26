SUSE/carrier

Add builder image arg to push

internal/cli/clients/client.go
@@ -52,8 +52,12 @@ type EpinioClient struct {
 }
 
 type PushParams struct {
-	Instances *int32
-	Services  []string
+	BuilderImage string
+	GitRevision  string
+	Instances    *int32
+	Name         string
+	Path         string
+	Services     []string
 }
 
 func NewEpinioClient(ctx context.Context, flags *pflag.FlagSet) (*EpinioClient, error) {
@@ -1261,7 +1265,11 @@ func (c *EpinioClient) Orgs() error {
 // * (tail logs)
 // * wait for pipelinerun
 // * wait for app
-func (c *EpinioClient) Push(ctx context.Context, name, rev, source string, params PushParams) error {
+func (c *EpinioClient) Push(ctx context.Context, params PushParams) error {
+	rev := params.GitRevision
+	name := params.Name
+	source := params.Path
+
 	appRef := models.AppRef{Name: name, Org: c.Config.Org}
 	log := c.Log.
 		WithName("Push").
@@ -1361,10 +1369,11 @@ func (c *EpinioClient) Push(ctx context.Context, name, rev, source string, param
 		return errors.Wrap(err, "unable to determine default app route")
 	}
 	req := models.StageRequest{
-		App:       appRef,
-		Instances: params.Instances,
-		Git:       gitRef,
-		Route:     route,
+		App:          appRef,
+		Instances:    params.Instances,
+		Git:          gitRef,
+		Route:        route,
+		BuilderImage: params.BuilderImage,
 	}
 	details.Info("staging code", "Git", gitRef.Revision)
 	stage, err := c.stageCode(req)

internal/cli/push.go
@@ -14,6 +14,7 @@ import (
 var ()
 
 func init() {
+	CmdPush.Flags().String("builder-image", "paketobuildpacks/builder:full", "paketo builder image to use for staging")
 	CmdPush.Flags().Int32P("instances", "i", v1.DefaultInstances,
 		"The number of desired instances for the application, default only applies to new deployments")
 	CmdPush.Flags().String("git", "", "git revision of sources. PATH becomes repository location")
@@ -95,6 +96,11 @@ var CmdPush = &cobra.Command{
 			return errors.Wrap(err, "could not read option --git")
 		}
 
+		builderImage, err := cmd.Flags().GetString("builderImage")
+		if err != nil {
+			return errors.Wrap(err, "could not read option --builder-image")
+		}
+
 		// Syntax:
 		// 1. push NAME
 		// 2. push NAME PATH
@@ -129,7 +135,11 @@ var CmdPush = &cobra.Command{
 			return errors.Wrap(err, "trouble with instances")
 		}
 		params := clients.PushParams{
-			Instances: i,
+			Name:         args[0],
+			Instances:    i,
+			GitRevision:  gitRevision,
+			Path:         path,
+			BuilderImage: builderImage,
 		}
 
 		services, err := cmd.Flags().GetStringSlice("bind")
@@ -138,7 +148,7 @@ var CmdPush = &cobra.Command{
 		}
 		params.Services = services
 
-		err = client.Push(cmd.Context(), args[0], gitRevision, path, params)
+		err = client.Push(cmd.Context(), params)
 		if err != nil {
 			return errors.Wrap(err, "error pushing app to server")
 		}

epinio/epinio

internal/cli/clients/client.go
@@ -51,10 +51,13 @@ type EpinioClient struct {
 }
 
 type PushParams struct {
-	Instances *int32
-	Services  []string
-	Docker    string
-	GitRev    string
+	Instances    *int32
+	Services     []string
+	Docker       string
+	GitRev       string
+	BuilderImage string
+	Name         string
+	Path         string
 }
 
 func NewEpinioClient(ctx context.Context) (*EpinioClient, error) {
@@ -1263,7 +1266,10 @@ func (c *EpinioClient) Orgs() error {
 // * wait for pipelinerun
 // * deploy
 // * wait for app
-func (c *EpinioClient) Push(ctx context.Context, name, source string, params PushParams) error {
+func (c *EpinioClient) Push(ctx context.Context, params PushParams) error {
+	name := params.Name
+	source := params.Path
+
 	appRef := models.AppRef{Name: name, Org: c.Config.Org}
 	log := c.Log.
 		WithName("Push").
@@ -1365,9 +1371,10 @@ func (c *EpinioClient) Push(ctx context.Context, name, source string, params Pus
 	if params.Docker == "" {
 		c.ui.Normal().Msg("Staging application ...")
 		req := models.StageRequest{
-			App:   appRef,
-			Git:   gitRef,
-			Route: route,
+			App:          appRef,
+			Git:          gitRef,
+			Route:        route,
+			BuilderImage: params.BuilderImage,
 		}
 		details.Info("staging code", "Git", gitRef.Revision)
 		stageResponse, err = c.stageCode(req)

internal/cli/push.go
@@ -14,6 +14,7 @@ import (
 var ()
 
 func init() {
+	CmdPush.Flags().String("builder-image", "paketobuildpacks/builder:full", "paketo builder image to use for staging")
 	CmdPush.Flags().Int32P("instances", "i", v1.DefaultInstances,
 		"The number of desired instances for the application, default only applies to new deployments")
 	CmdPush.Flags().String("git", "", "git revision of sources. PATH becomes repository location")
@@ -105,6 +106,11 @@ var CmdPush = &cobra.Command{
 			return errors.Wrap(err, "cannot use both, git and docker image url")
 		}
 
+		builderImage, err := cmd.Flags().GetString("builderImage")
+		if err != nil {
+			return errors.Wrap(err, "could not read option --builder-image")
+		}
+
 		// Syntax:
 		// 1. push NAME
 		// 2. push NAME PATH
@@ -144,9 +150,12 @@ var CmdPush = &cobra.Command{
 			return errors.Wrap(err, "trouble with instances")
 		}
 		params := clients.PushParams{
-			Instances: i,
-			GitRev:    gitRevision,
-			Docker:    dockerImageURL,
+			Name:         args[0],
+			Instances:    i,
+			GitRev:       gitRevision,
+			Docker:       dockerImageURL,
+			Path:         path,
+			BuilderImage: builderImage,
 		}
 
 		services, err := cmd.Flags().GetStringSlice("bind")
@@ -155,7 +164,7 @@ var CmdPush = &cobra.Command{
 		}
 		params.Services = services
 
-		err = client.Push(cmd.Context(), args[0], path, params)
+		err = client.Push(cmd.Context(), params)
 		if err != nil {
 			return errors.Wrap(err, "error pushing app to server")
 		}