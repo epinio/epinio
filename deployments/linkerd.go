1172cf7cbf8b69241f00a8f67e40726e443fc4ef
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2021-08-13 12:08:48 +0200

    Convert linkerd job creation into programmatical creation
    This commit also adds duration multiplier to linkerd check command

--- deployments/linkerd.go
+++ deployments/linkerd.go
@@ -8,9 +8,12 @@ import (
 	"github.com/epinio/epinio/helpers"
 	"github.com/epinio/epinio/helpers/kubernetes"
 	"github.com/epinio/epinio/helpers/termui"
+	"github.com/epinio/epinio/internal/duration"
 	"github.com/go-logr/logr"
 	"github.com/kyokomi/emoji"
 	"github.com/pkg/errors"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -29,6 +32,7 @@ const (
 	linkerdRolesYAML        = "linkerd/rbac.yaml"
 	linkerdInstallJobYAML   = "linkerd/install-job.yaml"
 	linkerdUninstallJobYAML = "linkerd/uninstall-job.yaml"
+	linkerdImage            = "splatform/epinio-linkerd"
 )
 
 func (k Linkerd) ID() string {
@@ -57,8 +61,10 @@ func (k Linkerd) Delete(ctx context.Context, c *kubernetes.Cluster, ui *termui.U
 	}
 
 	// Remove linkerd with the uninstall job
-	if out, err := helpers.KubectlApplyEmbeddedYaml(linkerdUninstallJobYAML); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Deleting %s failed:\n%s", linkerdUninstallJobYAML, out))
+	if err := k.createLinkerdJob(ctx, c, "linkerd-uninstall", "linkerd-installer",
+		fmt.Sprintf("%s:%s", linkerdImage, linkerdVersion),
+		"linkerd uninstall --verbose | kubectl delete -f -"); err != nil {
+		return errors.Wrapf(err, "creating linkerd uninstall job failed")
 	}
 
 	// The uninstall job also deletes the namespace.
@@ -78,6 +84,7 @@ func (k Linkerd) Delete(ctx context.Context, c *kubernetes.Cluster, ui *termui.U
 }
 
 func (k Linkerd) apply(ctx context.Context, c *kubernetes.Cluster, ui *termui.UI, options kubernetes.InstallationOptions, upgrade bool) error {
+	linkerdJobName := "linkerd-install"
 	if err := c.CreateNamespace(ctx, LinkerdDeploymentID, map[string]string{
 		kubernetes.EpinioDeploymentLabelKey: kubernetes.EpinioDeploymentLabelValue,
 	}, map[string]string{"linkerd.io/inject": "enabled"}); err != nil {
@@ -88,11 +95,13 @@ func (k Linkerd) apply(ctx context.Context, c *kubernetes.Cluster, ui *termui.UI
 		return errors.Wrap(err, fmt.Sprintf("Installing %s failed:\n%s", linkerdUninstallJobYAML, out))
 	}
 
-	if out, err := helpers.KubectlApplyEmbeddedYaml(linkerdInstallJobYAML); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Installing %s failed:\n%s", linkerdInstallJobYAML, out))
+	if err := k.createLinkerdJob(ctx, c, linkerdJobName, "linkerd-installer",
+		fmt.Sprintf("%s:%s", linkerdImage, linkerdVersion),
+		fmt.Sprintf("%s %s", "linkerd install | kubectl apply -f - && linkerd check --wait", duration.ToDeployment())); err != nil {
+		return errors.Wrapf(err, "installing linkerd installation job failed")
 	}
 
-	if err := c.WaitForJobCompleted(ctx, LinkerdDeploymentID, "linkerd-install", k.Timeout); err != nil {
+	if err := c.WaitForJobCompleted(ctx, LinkerdDeploymentID, linkerdJobName, k.Timeout); err != nil {
 		return errors.Wrap(err, "failed waiting Linkerd install job to complete")
 	}
 
@@ -177,4 +186,50 @@ func (k Linkerd) Upgrade(ctx context.Context, c *kubernetes.Cluster, ui *termui.
 	ui.Note().Msg("Upgrading Linkerd...")
 
 	return k.apply(ctx, c, ui, options, true)
+}
+
+func (k Linkerd) createLinkerdJob(ctx context.Context,
+	c *kubernetes.Cluster,
+	jobName,
+	serviceAccountName,
+	imageName,
+	jobCommand string) error {
+
+	backoffLimit := int32(1)
+
+	job := batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: jobName,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					ServiceAccountName: serviceAccountName,
+					Containers: []corev1.Container{
+						{
+							Name:            jobName,
+							Image:           imageName,
+							ImagePullPolicy: "Always",
+							Command: []string{
+								"/bin/sh",
+								"-c",
+							},
+							Args: []string{
+								jobCommand,
+							},
+						},
+					},
+					RestartPolicy: "Never",
+				},
+			},
+			BackoffLimit: &backoffLimit,
+		},
+	}
+
+	_, err := c.Kubectl.BatchV1().Jobs(LinkerdDeploymentID).Create(ctx, &job, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
