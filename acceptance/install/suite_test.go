fd410a9b5abcc0fc001f6ea7029eb6aa60e0e05e
Mario Manno <mario.manno@suse.com>
2021-08-30 15:52:08 +0200

    Fix patch script in AKS workflow

--- acceptance/install/suite_test.go
+++ acceptance/install/suite_test.go
@@ -37,7 +37,7 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	return []byte(strconv.Itoa(int(time.Now().Unix())))
 }, func(randomSuffix []byte) {
 	testenv.SetRoot("../..")
-	os.Setenv("EPINIO_CONFIG", testenv.EpinioYAML())
+	testenv.SetupEnv()
 
 	Expect(os.Getenv("KUBECONFIG")).ToNot(BeEmpty(), "KUBECONFIG environment variable should not be empty")
 })
