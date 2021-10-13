70ecf5f87f242af6711406ebeb4d642d9c852c30
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-10-13 14:32:01 +0300

    Use k3d v5.0.0 in CI (and development)

--- scripts/tools/k3d.sh
+++ scripts/tools/k3d.sh
@@ -3,13 +3,13 @@
 
 TOOLS+=(k3d)
 
-K3D_VERSION="4.4.4"
+K3D_VERSION="5.0.0"
 
 function k3d_version { k3d version; }
 
-K3D_SHA256_DARWIN="5fc9b68c9cd523ea743a9ca31163330db642d50f4db421ee00f7e1f4a29da552"
-K3D_SHA256_LINUX="6d4ac3d4c5b084f445980e427c5d3a75eefd2c39a22d028825c234c6c20d1e46"
-K3D_SHA256_WINDOWS="1e09e00ee830247ededc3c4718e62f89c1ec7a634fcdf9defb76d124e6a43b83"
+K3D_SHA256_DARWIN="e023e0153e08ad5e63a96783021138affbdfbde42c02eb060aaa846d60a9b76c"
+K3D_SHA256_LINUX="6744bfd5cea611dc3f2a24da7b25a28fd0dd4b78c486193c238d55619d7b7c43"
+K3D_SHA256_WINDOWS="0aa602e2d544e6a3200d0d7cded4eec9441658eb294526aca9d4262d1fe873de"
 
 K3D_URL_DARWIN="https://github.com/rancher/k3d/releases/download/v{version}/k3d-darwin-amd64"
 K3D_URL_LINUX="https://github.com/rancher/k3d/releases/download/v{version}/k3d-linux-amd64"
