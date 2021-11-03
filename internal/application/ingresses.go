8a00e5461937fc7e2894187353c0e55adbf94863
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-11-03 14:27:45 +0000

    Implement path based routing

--- internal/application/ingresses.go
+++ internal/application/ingresses.go
@@ -8,6 +8,7 @@ import (
 	"github.com/epinio/epinio/helpers/tracelog"
 	"github.com/epinio/epinio/internal/auth"
 	"github.com/epinio/epinio/internal/names"
+	"github.com/epinio/epinio/internal/routes"
 	apierror "github.com/epinio/epinio/pkg/api/core/v1/errors"
 	"github.com/epinio/epinio/pkg/api/core/v1/models"
 	"github.com/pkg/errors"
@@ -30,7 +31,12 @@ func ListRoutes(ctx context.Context, cluster *kubernetes.Cluster, appRef models.
 
 	result := []string{}
 	for _, ingress := range ingressList.Items {
-		result = append(result, ingress.Spec.Rules[0].Host)
+		route, err := routes.FromIngress(ingress)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, route.String())
 	}
 
 	return result, nil
@@ -71,7 +77,11 @@ func SyncIngresses(ctx context.Context, cluster *kubernetes.Cluster, appRef mode
 	// Construct a lookup-up map for existing ingresses
 	existingIngresses := map[string]networkingv1.Ingress{}
 	for _, ingress := range ingressList.Items {
-		existingIngresses[ingress.Spec.Rules[0].Host] = ingress
+		route, err := routes.FromIngress(ingress)
+		if err != nil {
+			return []string{}, err
+		}
+		existingIngresses[route.String()] = ingress
 	}
 
 	// Ensure desired routes
@@ -84,7 +94,10 @@ func SyncIngresses(ctx context.Context, cluster *kubernetes.Cluster, appRef mode
 		}
 		log.Info("creating app ingress", "org", appRef.Org, "app", appRef.Name, "", desiredRoute)
 
-		ing := newAppIngress(appRef, desiredRoute, username)
+		route := routes.FromString(desiredRoute)
+		ingressName := names.IngressName(fmt.Sprintf("%s-%s", appRef.Name, route))
+		ing := route.ToIngress(ingressName)
+		completeIngress(&ing, appRef, username) // Add more fields, annotations, etc
 
 		log.Info("app ingress", "name", ing.ObjectMeta.Name)
 
@@ -94,7 +107,7 @@ func SyncIngresses(ctx context.Context, cluster *kubernetes.Cluster, appRef mode
 		// If it doesn't exist, create the Ingress and the cert for it.
 		if _, err := cluster.Kubectl.NetworkingV1().Ingresses(appRef.Org).Get(ctx, ing.Name, metav1.GetOptions{}); err != nil {
 			if apierrors.IsNotFound(err) {
-				createdIngress, createErr := cluster.Kubectl.NetworkingV1().Ingresses(appRef.Org).Create(ctx, ing, metav1.CreateOptions{})
+				createdIngress, createErr := cluster.Kubectl.NetworkingV1().Ingresses(appRef.Org).Create(ctx, &ing, metav1.CreateOptions{})
 				if createErr != nil {
 					return []string{}, errors.Wrap(err, "creating an application Ingress")
 				}
@@ -104,7 +117,7 @@ func SyncIngresses(ctx context.Context, cluster *kubernetes.Cluster, appRef mode
 					Name:      createdIngress.Name,
 					Namespace: appRef.Org,
 					Issuer:    viper.GetString("tls-issuer"),
-					Domain:    desiredRoute,
+					Domain:    route.Domain,
 					Labels:    map[string]string{"app.kubernetes.io/name": appRef.Name},
 				}
 				certOwner := &metav1.OwnerReference{
@@ -141,65 +154,40 @@ func SyncIngresses(ctx context.Context, cluster *kubernetes.Cluster, appRef mode
 	return desiredRoutes, nil
 }
 
-// newAppIngress is a helper that creates the kube ingress resource for the app
-func newAppIngress(appRef models.AppRef, route, username string) *networkingv1.Ingress {
-	pathTypeImplementationSpecific := networkingv1.PathTypeImplementationSpecific
-
-	// name is used both for the Ingress name and the secret name
-	// We don't create the secret here, we just expect it to be called like that.
-	// The caller (SyncIngresses) makes sure the secret is created with the same
-	// name and has this Ingress as an owner.
-	name := names.IngressName(fmt.Sprintf("%s-%s", appRef.Name, route))
-	return &networkingv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-			Annotations: map[string]string{
-				"traefik.ingress.kubernetes.io/router.entrypoints": "websecure",
-				"traefik.ingress.kubernetes.io/router.tls":         "true",
-				"kubernetes.io/ingress.class":                      "traefik",
-			},
-			Labels: map[string]string{
-				"app.kubernetes.io/component":  "application",
-				"app.kubernetes.io/managed-by": "epinio",
-				"app.kubernetes.io/name":       appRef.Name,
-				"app.kubernetes.io/created-by": username,
-				"app.kubernetes.io/part-of":    appRef.Org,
-			},
-		},
-		Spec: networkingv1.IngressSpec{
-			Rules: []networkingv1.IngressRule{
-				{
-					Host: route,
-					IngressRuleValue: networkingv1.IngressRuleValue{
-						HTTP: &networkingv1.HTTPIngressRuleValue{
-							Paths: []networkingv1.HTTPIngressPath{
-								{
-									Backend: networkingv1.IngressBackend{
-										Service: &networkingv1.IngressServiceBackend{
-											Name: names.ServiceName(appRef.Name),
-											Port: networkingv1.ServiceBackendPort{
-												Number: 8080,
-											},
-										},
-									},
-									Path:     "/",
-									PathType: &pathTypeImplementationSpecific,
-								},
-							},
-						},
-					},
-				},
-			},
-			TLS: []networkingv1.IngressTLS{
-				{
-					Hosts: []string{
-						route,
-					},
-					SecretName: name + "-tls",
-				},
-			},
-		},
+// completeIngress that takes an Ingress as created by the routes#ToIngress
+// method and fills in more data needed for Epinio.
+func completeIngress(ingress *networkingv1.Ingress, appRef models.AppRef, username string) *networkingv1.Ingress {
+	ingress.ObjectMeta.Annotations = map[string]string{
+		"traefik.ingress.kubernetes.io/router.entrypoints": "websecure",
+		"traefik.ingress.kubernetes.io/router.tls":         "true",
+		"kubernetes.io/ingress.class":                      "traefik",
 	}
+
+	ingress.ObjectMeta.Labels = map[string]string{
+		"app.kubernetes.io/component":  "application",
+		"app.kubernetes.io/managed-by": "epinio",
+		"app.kubernetes.io/name":       appRef.Name,
+		"app.kubernetes.io/created-by": username,
+		"app.kubernetes.io/part-of":    appRef.Org,
+	}
+
+	ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend =
+		networkingv1.IngressBackend{
+			Service: &networkingv1.IngressServiceBackend{
+				Name: names.ServiceName(appRef.Name),
+				Port: networkingv1.ServiceBackendPort{
+					Number: 8080,
+				}}}
+
+	ingress.Spec.TLS = []networkingv1.IngressTLS{{
+		Hosts: []string{ingress.Spec.Rules[0].Host},
+		// 2 different applications using the same domain, will use a different
+		// certificate each. This simplifies the management of certificates and
+		// cascading cleanups (application deletion automatically deletes certs).
+		SecretName: ingress.Name + "-tls", // Secret has the same name as the Ingress
+	}}
+
+	return ingress
 }
 
 func ingressListForApp(ctx context.Context, cluster *kubernetes.Cluster, appRef models.AppRef) (*networkingv1.IngressList, error) {
