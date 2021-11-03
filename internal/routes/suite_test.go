0c9675660897b9d1aae90607cbc938dca781bf08
Dimitris Karakasilis <DKarakasilis@suse.com>
2021-11-03 09:34:46 +0000

    Introduce routes package

---
+++ internal/routes/suite_test.go
@@ -0,0 +1,13 @@
+package routes_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestEpinio(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Epinio routes suite")
+}
