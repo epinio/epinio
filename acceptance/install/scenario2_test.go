82442dc30ed72d27bff823775de7d0151dcb493f
Loic Devulder <ldevulder@suse.com>
2021-11-05 12:21:32 +0100

    Find certificate with selector - part 2
    Add the fix also for AKS and GKE.

--- acceptance/install/scenario2_test.go
+++ acceptance/install/scenario2_test.go
@@ -123,7 +123,9 @@ var _ = Describe("<Scenario2>", func() {
 
 			// Verify cluster_issuer is used
 			out, err = proc.RunW("kubectl", "get", "certificate",
-				"-n", testenv.DefaultWorkspace, appName, "-o", "jsonpath='{.spec.issuerRef.name}'")
+				"-n", testenv.DefaultWorkspace,
+				"--selector", "app.kubernetes.io/name="+appName,
+				"-o", "jsonpath='{.items[*].spec.issuerRef.name}'")
 			Expect(err).NotTo(HaveOccurred(), out)
 			Expect(out).To(Equal("'letsencrypt-production'"))
 		})
