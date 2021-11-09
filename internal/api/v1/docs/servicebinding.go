3823396d98b89072a34d16b00d2033a792d69f4f
Andreas Kupries <andreas.kupries@suse.com>
2021-11-09 12:05:02 +0100

    feat: swagger annotations for service binding API.

---
+++ internal/api/v1/docs/servicebinding.go
@@ -0,0 +1,42 @@
+package docs
+
+import "github.com/epinio/epinio/pkg/api/core/v1/models"
+
+// Service Bindings
+
+// swagger:route POST   /namespaces/{namespace}/applications/{app}/servicebindings svc-binding ServiceBindingCreate
+// Create service binding between `app` in `namespace`, and the posted services, also in `namespace`.
+// responses:
+//   200: ServiceBindResponse
+
+// swagger:parameters ServiceBindingCreate
+type ServiceBindingCreateParams struct {
+	Namespace string
+	App       string
+	// in: body
+	Body models.BindRequest
+}
+
+// swagger:response ServiceBindResponse
+type ServiceBindResponse struct {
+	// in: body
+	Body models.BindResponse
+}
+
+// swagger:route DELETE /namespaces/{namespace}/applications/{app}/servicebindings/{service} svc-binding ServiceBindingDelete
+// Remove service binding between `app` and `service` in `namespace`.
+// responses:
+//   200: ServiceUnbindReponse
+
+// swagger:parameters ServiceBindingDelete
+type ServiceBindingDeleteParams struct {
+	Namespace string
+	App       string
+	Service   string
+}
+
+// swagger:response ServiceUnbindReponse
+type ServiceUnbindReponse struct {
+	// in:body
+	Body models.Response
+}
