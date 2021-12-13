ce6aa213eb62dc93fab09891dc2c84782131ab01
Loic Devulder <ldevulder@suse.com>
2021-12-13 17:11:13 +0100

    RKE-CI: get a free IP address on server's network
    ... instead of using an arbitrary one.

--- acceptance/install/scenario3_test.go
+++ acceptance/install/scenario3_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -21,18 +22,22 @@ var _ = Describe("<Scenario3> RKE, Private CA, Service, on External Registry", f
 		serviceName      = catalog.NewServiceName()
 		appName          string
 		loadbalancer     string
-		localpathURL     string
 		registryUsername string
 		registryPassword string
+		rangeIP          string
+		domainIP         []string
 		// testenv.New is not needed for VerifyAppServiceBound helper :shrug:
-		env      testenv.EpinioEnv
-		domainIP = "192.168.1.240" // Set it to an arbitrary private IP
+		env          testenv.EpinioEnv
+		localpathURL = "https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.20/deploy/local-path-storage.yaml"
 	)
 
 	BeforeEach(func() {
 		epinioHelper = epinio.NewEpinioHelper(testenv.EpinioBinaryPath())
 
-		localpathURL = "https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.20/deploy/local-path-storage.yaml"
+		// Get a free IP address on server's network
+		rangeIP, _ = proc.Run(testenv.Root(), false, "bash", "./scripts/get-free-ip.sh")
+		domainIP = strings.Split(rangeIP, "-")
+
 		appName = "externalregtest"
 
 		registryUsername = os.Getenv("REGISTRY_USERNAME")
@@ -42,7 +47,7 @@ var _ = Describe("<Scenario3> RKE, Private CA, Service, on External Registry", f
 		Expect(registryPassword).ToNot(BeEmpty())
 		flags = []string{
 			"--set", "skipCertManager=true",
-			"--set", "domain=" + fmt.Sprintf("%s.omg.howdoi.website", domainIP),
+			"--set", "domain=" + fmt.Sprintf("%s.omg.howdoi.website", domainIP[0]),
 			"--set", "tlsIssuer=private-ca",
 			"--set", "externalRegistryURL=registry.hub.docker.com",
 			"--set", "externalRegistryUsername=" + registryUsername,
@@ -55,20 +60,23 @@ var _ = Describe("<Scenario3> RKE, Private CA, Service, on External Registry", f
 	AfterEach(func() {
 		out, err := epinioHelper.Uninstall()
 		Expect(err).NotTo(HaveOccurred(), out)
+
+		out, err = proc.RunW("helm", "uninstall", "-n", "metallb", "metallb")
+		Expect(err).NotTo(HaveOccurred(), out)
 	})
 
 	It("installs with private CA and pushes an app with service", func() {
 		By("Installing MetalLB", func() {
-			out, err := proc.RunW("helm", "repo", "add", "metallb", "https://metallb.github.io/metallb")
+			out, err := proc.RunW("sed", "-i", fmt.Sprintf("s/@IP_RANGE@/%s/", rangeIP),
+				testenv.TestAssetPath("values-metallb-rke.yml"))
 			Expect(err).NotTo(HaveOccurred(), out)
 
-			out, err = proc.RunW("helm", "upgrade", "--install", "-n", "metallb-system", "--create-namespace", "metallb", "metallb/metallb")
+			out, err = proc.RunW("helm", "repo", "add", "metallb", "https://metallb.github.io/metallb")
 			Expect(err).NotTo(HaveOccurred(), out)
 
-			out, err = proc.RunW("sed", "-i", fmt.Sprintf("s/myip/%s/g", domainIP), testenv.TestAssetPath("config-metallb-rke.yml"))
-			Expect(err).NotTo(HaveOccurred(), out)
-
-			out, err = proc.RunW("kubectl", "apply", "-f", testenv.TestAssetPath("config-metallb-rke.yml"))
+			out, err = proc.RunW("helm", "upgrade", "--install", "-n", "metallb",
+				"--create-namespace", "metallb", "metallb/metallb", "-f",
+				testenv.TestAssetPath("values-metallb-rke.yml"))
 			Expect(err).NotTo(HaveOccurred(), out)
 		})
 
@@ -124,12 +132,15 @@ var _ = Describe("<Scenario3> RKE, Private CA, Service, on External Registry", f
 			Expect(status.Status.LoadBalancer.Ingress).To(HaveLen(1))
 			loadbalancer = status.Status.LoadBalancer.Ingress[0].IP
 			Expect(loadbalancer).ToNot(BeEmpty())
+			// We need to be sure that the specified IP is really used
+			Expect(loadbalancer).To(Equal(domainIP[0]))
 		})
 
 		By("Checking Epinio info command", func() {
-			out, err := epinioHelper.Run("info")
-			Expect(err).NotTo(HaveOccurred(), out)
-			Expect(out).To(ContainSubstring("Epinio Version:"))
+			Eventually(func() string {
+				out, _ := epinioHelper.Run("info")
+				return out
+			}, "2m", "2s").Should(ContainSubstring("Epinio Version:"))
 		})
 
 		By("Creating a service and pushing an app", func() {
