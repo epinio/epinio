e2811b8d4f9ee3a9b9ef5c396acfc35653102360
Tim Hardeck <thardeck@suse.com>
2021-12-16 10:07:32 +0100

    Revert "TODO added regarding Create retries on cli side"
    This reverts commit a0f77f4744228fc00337be511ff6f6005e3e97a3.

--- internal/api/v1/namespace/create.go
+++ internal/api/v1/namespace/create.go
@@ -38,7 +38,6 @@ func (oc Controller) Create(c *gin.Context) apierror.APIErrors {
 		return apierror.InternalError(err)
 	}
 	if exists {
-		// TODO: Don't retry when this happens (on the cli)
 		return apierror.NamespaceAlreadyKnown(request.Name)
 	}
 
