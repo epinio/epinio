b21d7b1b3e817dea9dbe2f61101d910244dc4e19
Dimitris Karakasilis <DKarakasilis@suse.com>
2022-01-14 12:56:44 +0000

    Remove installer Dockerfile
    because it is built here:
    
    https://github.com/epinio/installer/blob/main/Dockerfile
    https://github.com/epinio/installer/blob/main/.goreleaser.yml#L44

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -5,7 +5,6 @@ set -e
 version="$(git describe --tags)"
 base_image="splatform/epinio-base"
 server_image="splatform/epinio-server"
-installer_image="splatform/epinio-installer"
 
 # Build base image
 docker build -t "${base_image}:${version}" -t "${base_image}:latest" -f images/baseimage-Dockerfile .
@@ -16,9 +15,3 @@ docker push "${base_image}:latest"
 docker build -t "${server_image}:${version}" -t "${server_image}:latest" --build-arg BASE_IMAGE=${base_image} -f images/Dockerfile .
 docker push "${server_image}:${version}"
 docker push "${server_image}:latest"
-
-# Build installer image
-# This image is used by the Epinio helm chart
-docker build -t "${installer_image}:${version}" -t "${installer_image}:latest" -f images/installer-Dockerfile .
-docker push "${installer_image}:${version}"
-docker push "${installer_image}:latest"
