d2fab31dde5656b2dbc0a384b7f23a425a37034e
Loic Devulder <ldevulder@suse.com>
2022-01-27 21:49:39 +0100

    Remove unsed function 'assetPath' from helpers

--- helpers/helpers_suite_test.go
+++ helpers/helpers_suite_test.go
@@ -1,8 +1,6 @@
 package helpers_test
 
 import (
-	"os"
-	"path"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -12,14 +10,4 @@ import (
 func TestHelpers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Helpers Suite")
-}
-
-// assetPath returns the full path to a file in "fixtures" directory
-func assetPath(fileName string) string {
-	currentPath, err := os.Getwd()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return path.Join(currentPath, "..", "assets", "tests", fileName)
 }
