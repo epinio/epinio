3c75d816a6ebf7b994b1a76185dbc32eb7cd3171
Tim Hardeck <thardeck@suse.com>
2022-02-14 09:05:56 +0100

    make build-images should not push to Docker Hub
    But when using k3d for example an import is needed to make the internal
    Docker image available to k3d.

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -8,10 +8,6 @@ server_image="splatform/epinio-server"
 
 # Build base image
 docker build -t "${base_image}:${version}" -t "${base_image}:latest" -f images/baseimage-Dockerfile .
-docker push "${base_image}:${version}"
-docker push "${base_image}:latest"
 
 # Build server image
 docker build -t "${server_image}:${version}" -t "${server_image}:latest" --build-arg BASE_IMAGE=${base_image} -f images/Dockerfile .
-docker push "${server_image}:${version}"
-docker push "${server_image}:latest"
