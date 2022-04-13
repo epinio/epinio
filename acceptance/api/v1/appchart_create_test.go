4ed3392cafb8a75f906b86c9f5d1c075ac9e5612
Andreas Kupries <andreas.kupries@suse.com>
2022-04-13 14:35:37 +0200

    fixup oops - committed a focus

--- acceptance/api/v1/appchart_create_test.go
+++ acceptance/api/v1/appchart_create_test.go
@@ -29,7 +29,7 @@ var _ = Describe("ChartCreate Endpoint", func() {
 			Expect(err).ToNot(HaveOccurred(), out)
 		})
 
-		FIt("fails to create the chart", func() {
+		It("fails to create the chart", func() {
 			request := models.ChartCreateRequest{
 				Name:      "duplicate.api",
 				HelmChart: "placeholder",
