953672b52d01f65434182a4e328f29377adf3b2d
Andreas Kupries <andreas.kupries@suse.com>
2022-04-13 15:09:13 +0200

    tests: api, chart match

---
+++ acceptance/api/v1/appchart_match_test.go
@@ -0,0 +1,58 @@
+package v1_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	v1 "github.com/epinio/epinio/internal/api/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ChartMatch Endpoints", func() {
+
+	It("lists the app chart names matching the prefix, none", func() {
+		response, err := env.Curl("GET", fmt.Sprintf("%s%s/appchartsmatch/fox",
+			serverURL, v1.Root), strings.NewReader(""))
+		Expect(err).ToNot(HaveOccurred())
+		Expect(response).ToNot(BeNil())
+
+		defer response.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		Expect(response.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
+
+		Expect(err).ToNot(HaveOccurred())
+		Expect(string(bodyBytes)).To(Equal(`{}`))
+	})
+
+	It("lists the app chart names matching the prefix, standard", func() {
+		response, err := env.Curl("GET", fmt.Sprintf("%s%s/appchartsmatch/stan",
+			serverURL, v1.Root), strings.NewReader(""))
+		Expect(err).ToNot(HaveOccurred())
+		Expect(response).ToNot(BeNil())
+
+		defer response.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		Expect(response.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
+
+		Expect(err).ToNot(HaveOccurred())
+		Expect(string(bodyBytes)).To(Equal(`{"names":["standard"]}`))
+	})
+
+	It("lists the app chart names matching the prefix, all", func() {
+		response, err := env.Curl("GET", fmt.Sprintf("%s%s/appchartsmatch",
+			serverURL, v1.Root), strings.NewReader(""))
+		Expect(err).ToNot(HaveOccurred())
+		Expect(response).ToNot(BeNil())
+
+		defer response.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		Expect(response.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
+
+		Expect(err).ToNot(HaveOccurred())
+		Expect(string(bodyBytes)).To(Equal(`{"names":["standard"]}`))
+	})
+})
