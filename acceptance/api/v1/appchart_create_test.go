1a1d5adb3295165bb346142221f2755711d40921
Andreas Kupries <andreas.kupries@suse.com>
2022-04-13 12:24:52 +0200

    tests: api, app chart create

---
+++ acceptance/api/v1/appchart_create_test.go
@@ -0,0 +1,69 @@
+package v1_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/epinio/epinio/acceptance/helpers/proc"
+	v1 "github.com/epinio/epinio/internal/api/v1"
+	"github.com/epinio/epinio/pkg/api/core/v1/models"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ChartCreate Endpoint", func() {
+
+	standardBall := "https://github.com/epinio/helm-charts/releases/download/epinio-application-0.1.15/epinio-application-0.1.15.tgz"
+
+	When("creating a new app chart", func() {
+		AfterEach(func() {
+			out, err := env.Epinio("", "apps", "chart", "delete", "standard.direct")
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		It("creates the app chart resource", func() {
+			request := models.ChartCreateRequest{
+				Name:        "standard.direct",
+				Description: "Direct standard",
+				ShortDesc:   "Direct url to tarball standard",
+				HelmChart:   standardBall,
+			}
+			b, err := json.Marshal(request)
+			Expect(err).ToNot(HaveOccurred())
+
+			url := serverURL + v1.Root + "/" + v1.Routes.Path("ChartCreate")
+			response, err := env.Curl("POST", url, strings.NewReader(string(b)))
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response).ToNot(BeNil())
+			defer response.Body.Close()
+
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusCreated), string(bodyBytes))
+
+			out, err := proc.Kubectl("get", "appcharts", "-n", "epinio",
+				"standard.direct", "-o", "jsonpath={.spec.description}")
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).To(Equal("Direct standard"))
+
+			out, err = proc.Kubectl("get", "appcharts", "-n", "epinio",
+				"standard.direct", "-o", "jsonpath={.spec.shortDescription}")
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).To(Equal("Direct url to tarball standard"))
+
+			out, err = proc.Kubectl("get", "appcharts", "-n", "epinio",
+				"standard.direct", "-o", "jsonpath={.spec.helmChart}")
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).To(Equal(standardBall))
+
+			out, err = proc.Kubectl("get", "appcharts", "-n", "epinio",
+				"standard.direct", "-o", "jsonpath={.spec.helmRepo}")
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).To(Equal(""))
+		})
+	})
+})
