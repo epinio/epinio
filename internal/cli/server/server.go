2b244cfce24f7eda1488df27db5c2cd6578a777c
Enrico Candino <enrico.candino@suse.com>
2022-04-28 18:04:28 +0200

    added Epinio User

--- internal/cli/server/server.go
+++ internal/cli/server/server.go
@@ -178,7 +178,6 @@ func authMiddleware(ctx *gin.Context) {
 				response.Error(ctx, apierrors.NewInternalError("Couldn't extract user from the auth header"))
 				ctx.Abort()
 				return
-			}
 		}
 
 		// Perform basic auth authentication
