8555ec226af2c1897d4e3b66a5b4afbf357dddb4
Enrico Candino <enrico.candino@suse.com>
2022-05-03 15:35:46 +0200

    fixed comment

--- internal/cli/server/requestctx/context.go
+++ internal/cli/server/requestctx/context.go
@@ -18,12 +18,12 @@ type UserKey struct{}
 // LoggerKey is the unique key to lookup the logger from the request's context
 type LoggerKey struct{}
 
-// WithUser adds the user name to the context
+// WithUser adds the User to the context
 func WithUser(ctx context.Context, val auth.User) context.Context {
 	return context.WithValue(ctx, UserKey{}, val)
 }
 
-// User returns the user name from the context
+// User returns the User from the context
 func User(ctx context.Context) auth.User {
 	user, ok := ctx.Value(UserKey{}).(auth.User)
 	if !ok {
@@ -39,7 +39,11 @@ func WithID(ctx context.Context, val string) context.Context {
 
 // ID returns the request ID from the context
 func ID(ctx context.Context) string {
-	return extractString(ctx, IDKey{})
+	id, ok := ctx.Value(IDKey{}).(string)
+	if !ok {
+		return ""
+	}
+	return id
 }
 
 // WithLogger returns a copy of the context with the given logger
@@ -55,12 +59,4 @@ func Logger(ctx context.Context) logr.Logger {
 		return tracelog.NewLogger().WithName("fallback")
 	}
 	return log
-}
-
-func extractString(ctx context.Context, key interface{}) string {
-	val, ok := ctx.Value(key).(string)
-	if ok {
-		return val
-	}
-	return ""
 }
