d1a729b2fbe88fb827caef31bd9996095361cc7e
Enrico Candino <enrico.candino@suse.com>
2022-05-25 16:42:09 +0200

    fixed API tests

--- acceptance/helpers/machine/curl.go
+++ acceptance/helpers/machine/curl.go
@@ -2,12 +2,12 @@ package machine
 
 import (
 	"crypto/tls"
+	"io"
 	"net/http"
-	"strings"
 )
 
 // Curl is used to make requests against a server
-func (m *Machine) Curl(method, uri string, requestBody *strings.Reader) (*http.Response, error) {
+func (m *Machine) Curl(method, uri string, requestBody io.Reader) (*http.Response, error) {
 	request, err := http.NewRequest(method, uri, requestBody)
 	if err != nil {
 		return nil, err
