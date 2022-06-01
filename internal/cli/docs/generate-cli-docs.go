d6a1ebc8527708ba978b54d70559679eaecc0cf8
Enrico Candino <enrico.candino@suse.com>
2022-06-01 15:37:14 +0200

    updated cli doc generator

--- internal/cli/docs/generate-cli-docs.go
+++ internal/cli/docs/generate-cli-docs.go
@@ -3,40 +3,32 @@ package main
 import (
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/epinio/epinio/internal/cli"
-	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
-var markdownURLsToDocsy = func(s string) string {
-	s = strings.ReplaceAll(s, ".md", "")
-	s = "../" + s
-	return s
-}
-
-func docsyPrepend(s, docDir string) string {
-	title := strings.ReplaceAll(s, docDir, "")
-	title = strings.ReplaceAll(title, ".md", "")
-	title = strings.ReplaceAll(title, "_", " ")
-
-	return `---
-title: "` + title + `"
-linkTitle: "` + title + `"
-weight: 1
----
-`
-}
-
-func genCLIDocsyMarkDown(cmd *cobra.Command, docDir string) error {
-	cmd.DisableAutoGenTag = true
-	return doc.GenMarkdownTreeCustom(cmd, filepath.Join("./", docDir), func(s string) string { return docsyPrepend(s, docDir) }, markdownURLsToDocsy)
-}
-
 func main() {
-	docDir := os.Args[1]
-	if err := genCLIDocsyMarkDown(cli.NewEpinioCLI(), docDir); err != nil {
+	docDir := filepath.Join("./", os.Args[1])
+
+	cmd := cli.NewEpinioCLI()
+	cmd.DisableAutoGenTag = true
+
+	err := doc.GenMarkdownTreeCustom(cmd, docDir, filePrepender(docDir), linkHandler)
+	if err != nil {
 		panic(err)
 	}
+}
+
+func filePrepender(docDir string) func(string) string {
+	return func(file string) string {
+		return "---\ntitle: \"\"\n---\n"
+	}
+}
+
+func linkHandler(link string) string {
+	if link == "epinio_app_push.md" {
+		link = "epinio_push.md"
+	}
+	return "./" + link
 }
--- scripts/cli-docs-generate.sh
+++ scripts/cli-docs-generate.sh
@@ -13,18 +13,5 @@ go run internal/cli/docs/generate-cli-docs.go "${destination}"/
 # Fix ${HOME} references to proper `~`.
 perl -pi -e "s@${HOME}@~@" "${destination}"/*md
 
-# Fix the cross-links to match hierarchy and tooling expectations.
-sed -i 's@(\.\./\(.*\))@(./\1.md)@' "${destination}"/*md
-
-# Rework the YAML header/annotations. Fix `push` link.
-for md in "${destination}"/*md
-do
-    echo -n .
-    ( cat $md | grep -v linkTitle: | grep -v weight:
-    ) | sed -e 's|epinio_app_push.md|epinio_push.md|' \
-      > $$
-    mv $$ $md
-done
-
 echo /Done
 exit
