f6b53e640b62f6a22d1974f268e8d99e0395275c
Dimitris Karakasilis <DKarakasilis@suse.com>
2022-07-05 18:19:03 +0300

    Check content type and validate

--- acceptance/api/v1/application_upload_test.go
+++ acceptance/api/v1/application_upload_test.go
@@ -61,6 +61,29 @@ var _ = Describe("AppUpload Endpoint", func() {
 		})
 	})
 
+	When("uploading a zip file", func() {
+		BeforeEach(func() {
+			path = testenv.TestAssetPath("sample-app.zip")
+		})
+
+		It("returns the app response", func() {
+			resp, err := env.Client().Do(request)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp).ToNot(BeNil())
+			defer resp.Body.Close()
+
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
+
+			r := &models.UploadResponse{}
+			err = json.Unmarshal(bodyBytes, &r)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(r.BlobUID).ToNot(BeEmpty())
+		})
+	})
+
 	When("uploading a non-supported archive type", func() {
 		BeforeEach(func() {
 			path = testenv.TestAssetPath("sample-app.rar")
