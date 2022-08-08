d7ee702888352127de652f51c9a55461ce4690fc
Manuel Martín <manuel.martin@suse.com>
2022-08-08 15:10:09 +0200

    adding go scenario for rd. Improving rd yaml script

---
+++ acceptance/install/scenario7_test.go
@@ -0,0 +1,142 @@
+package install_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/epinio/epinio/acceptance/helpers/catalog"
+	"github.com/epinio/epinio/acceptance/helpers/epinio"
+	"github.com/epinio/epinio/acceptance/helpers/proc"
+	"github.com/epinio/epinio/acceptance/testenv"
+)
+
+var _ = Describe("<Scenario7> Rancher Desktop install and push epinio on self hosted runner", func() {
+	var (
+		flags             []string
+		epinioHelper      epinio.Epinio
+		configurationName = catalog.NewConfigurationName()
+		appName           string
+		loadbalancer      string
+		registryUsername  string
+		registryPassword  string
+		rangeIP           string
+		domain            string
+		domainIP          string
+		// testenv.New is not needed for VerifyAppConfigurationBound helper :shrug:
+		env          testenv.EpinioEnv
+		localpathURL = "https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.20/deploy/local-path-storage.yaml"
+	)
+
+	BeforeEach(func() {
+		epinioHelper = epinio.NewEpinioHelper(testenv.EpinioBinaryPath())
+
+		// Clean previous installed helm repos
+		// Done at the beginning because we don't know the runner's state
+		out, err := proc.Run(testenv.Root(), false, "bash", "./scripts/remove-helm-repos.sh")
+		Expect(err).NotTo(HaveOccurred(), out)
+
+		domain = os.Getenv("EPINIO_SYSTEM_DOMAIN")
+		Expect(domain).ToNot(BeEmpty())
+		domainIP = strings.TrimSuffix(domain, ".omg.howdoi.website")
+
+		appName = "external-reg-test-rke"
+
+		registryUsername = os.Getenv("REGISTRY_USERNAME")
+		Expect(registryUsername).ToNot(BeEmpty())
+
+		registryPassword = os.Getenv("REGISTRY_PASSWORD")
+		Expect(registryPassword).ToNot(BeEmpty())
+		flags = []string{
+			"--set", "global.domain=" + domain,
+			"--set", "global.tlsIssuer=private-ca",
+			"--set", "containerregistry.enabled=false",
+			"--set", "global.registryURL=registry.hub.docker.com",
+			"--set", "global.registryUsername=" + registryUsername,
+			"--set", "global.registryPassword=" + registryPassword,
+			"--set", "global.registryNamespace=splatform",
+		}
+
+	})
+
+	AfterEach(func() {
+		out, err := epinioHelper.Uninstall()
+		Expect(err).NotTo(HaveOccurred(), out)
+	})
+
+	It("Install Epinio in Rancher Desktop and push app", func() {
+		By("Checking LoadBalancer IP", func() {
+			// Ensure that Traefik LB is not in Pending state anymore, could take time
+			Eventually(func() string {
+				out, err := proc.RunW("kubectl", "get", "svc", "-n", "traefik", "traefik", "--no-headers")
+				Expect(err).NotTo(HaveOccurred(), out)
+				return out
+			}, "4m", "2s").ShouldNot(ContainSubstring("<pending>"))
+
+			out, err := proc.RunW("kubectl", "get", "service", "-n", "traefik", "traefik", "-o", "json")
+			Expect(err).NotTo(HaveOccurred(), out)
+
+			// Check that an IP address for LB is configured
+			status := &testenv.LoadBalancerHostname{}
+			err = json.Unmarshal([]byte(out), status)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status.Status.LoadBalancer.Ingress).To(HaveLen(1))
+			loadbalancer = status.Status.LoadBalancer.Ingress[0].IP
+			Expect(loadbalancer).ToNot(BeEmpty())
+		})
+
+		By("Installing Epinio", func() {
+			out, err := epinioHelper.Install(flags...)
+			Expect(err).NotTo(HaveOccurred(), out)
+			Expect(out).To(ContainSubstring("STATUS: deployed"))
+
+			out, err = testenv.PatchEpinio()
+			Expect(err).ToNot(HaveOccurred(), out)
+		})
+
+		By("Connecting to Epinio", func() {
+			Eventually(func() string {
+				out, _ := epinioHelper.Run("login", "-u", "admin", "-p", "password", "--trust-ca", "https://epinio."+domain)
+				return out
+			}, "2m", "5s").Should(ContainSubstring("Login successful"))
+		})
+
+		By("Checking Epinio info command", func() {
+			Eventually(func() string {
+				out, _ := epinioHelper.Run("info")
+				return out
+			}, "2m", "2s").Should(ContainSubstring("Epinio Server Version:"))
+		})
+
+		By("Creating a configuration and pushing an app", func() {
+			out, err := epinioHelper.Run("configuration", "create", configurationName, "mariadb", "10-3-22")
+			Expect(err).NotTo(HaveOccurred(), out)
+
+			out, err = epinioHelper.Run("push",
+				"--name", appName,
+				"--path", testenv.AssetPath("sample-app"),
+				"--bind", configurationName)
+			Expect(err).NotTo(HaveOccurred(), out)
+
+			env.VerifyAppConfigurationBound(appName, configurationName, testenv.DefaultWorkspace, 1)
+
+			// Verify cluster_issuer is used
+			out, err = proc.RunW("kubectl", "get", "certificate",
+				"-n", testenv.DefaultWorkspace,
+				"--selector", "app.kubernetes.io/name="+appName,
+				"-o", "jsonpath='{.items[*].spec.issuerRef.name}'")
+			Expect(err).NotTo(HaveOccurred(), out)
+			Expect(out).To(Equal("'private-ca'"))
+		})
+
+		By("Delete an app", func() {
+			out, err := epinioHelper.Run("apps", "delete", appName)
+			Expect(err).NotTo(HaveOccurred(), out)
+			Expect(out).To(Or(ContainSubstring("Application deleted")))
+		})
+	})
+})
