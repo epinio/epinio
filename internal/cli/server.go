72034ae3ecc9b0db0d42513d6051437a0a9dcb95
Enrico Candino <enrico.candino@suse.com>
2022-08-19 14:37:39 +0200

    fix lint

--- internal/cli/server.go
+++ internal/cli/server.go
@@ -91,7 +91,8 @@ var CmdServer = &cobra.Command{
 // startServerGracefully will start the server and will wait for a graceful shutdown
 func startServerGracefully(listener net.Listener, handler http.Handler) error {
 	srv := &http.Server{
-		Handler: handler,
+		ReadHeaderTimeout: time.Second * 30,
+		Handler:           handler,
 	}
 
 	quit := make(chan os.Signal, 1)
