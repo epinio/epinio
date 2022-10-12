ea7129f2212ba3eac0902c98cb61737d8f7cc791
Enrico Candino <enrico.candino@suse.com>
2022-10-12 14:39:40 +0200

    Added internal Dex endpoint for Epinio server (#1787)
    * added internal dex endpoint for server
    
    * check internal endpoint

--- internal/dex/dex.go
+++ internal/dex/dex.go
@@ -2,6 +2,8 @@ package dex
 
 import (
 	"context"
+	"net/url"
+	"strings"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/dchest/uniuri"
@@ -22,13 +24,33 @@ var (
 
 // OIDCProvider wraps an oidc.Provider and its Configuration
 type OIDCProvider struct {
+	Issuer   string
+	Endpoint *url.URL
 	Provider *oidc.Provider
 	Config   *oauth2.Config
 }
 
 // NewOIDCProvider construct an OIDCProvider fetching its configuration
 func NewOIDCProvider(ctx context.Context, issuer, clientID string) (*OIDCProvider, error) {
-	provider, err := oidc.NewProvider(ctx, issuer)
+	endpoint, err := url.Parse(issuer)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing the issuer URL")
+	}
+
+	return NewOIDCProviderWithEndpoint(ctx, issuer, clientID, endpoint)
+}
+
+// NewOIDCProviderWithEndpoint construct an OIDCProvider fetching its configuration from the endpoint URL
+func NewOIDCProviderWithEndpoint(ctx context.Context, issuer, clientID string, endpoint *url.URL) (*OIDCProvider, error) {
+	// If the issuer is different from the endpoint we need to set it in the context.
+	// With this differentiation the Epinio server can reach the Dex service through the Kubernetes DNS
+	// instead of the external URL. This was causing issues when the host was going to be resolved as a local IP (i.e: Rancher Desktop).
+	// - https://github.com/epinio/epinio/issues/1781
+	if issuer != endpoint.String() && strings.HasSuffix(endpoint.Hostname(), ".svc.cluster.local") {
+		ctx = oidc.InsecureIssuerURLContext(ctx, issuer)
+	}
+
+	provider, err := oidc.NewProvider(ctx, endpoint.String())
 	if err != nil {
 		return nil, errors.Wrap(err, "creating the provider")
 	}
@@ -41,6 +63,8 @@ func NewOIDCProvider(ctx context.Context, issuer, clientID string) (*OIDCProvide
 	}
 
 	return &OIDCProvider{
+		Issuer:   issuer,
+		Endpoint: endpoint,
 		Provider: provider,
 		Config:   config,
 	}, nil
@@ -79,7 +103,8 @@ func (pc *OIDCProvider) ExchangeWithPKCE(ctx context.Context, authCode, codeVeri
 
 // Verify will verify the token, and it will return an oidc.IDToken
 func (pc *OIDCProvider) Verify(ctx context.Context, rawIDToken string) (*oidc.IDToken, error) {
-	verifier := pc.Provider.Verifier(&oidc.Config{ClientID: pc.Config.ClientID})
+	keySet := oidc.NewRemoteKeySet(ctx, pc.Endpoint.String()+"/keys")
+	verifier := oidc.NewVerifier(pc.Issuer, keySet, &oidc.Config{ClientID: pc.Config.ClientID})
 
 	token, err := verifier.Verify(ctx, rawIDToken)
 	if err != nil {
