0cae89b853bdb0c2b602218dfc92b183fdf7c945
Manuel Martín <manuel.martin@suse.com>
2022-11-21 15:49:04 +0100

    Applying formatting

--- acceptance/upgrade/upgrade_test.go
+++ acceptance/upgrade/upgrade_test.go
@@ -145,10 +145,10 @@ var _ = Describe("Epinio upgrade with running app", func() {
 				WithRow("Status", "deployed"),
 			),
 		)
-		time.Sleep(25*time.Second)
+		time.Sleep(25 * time.Second)
 
 		// Deploy Wordpress application
-		By("Pushing Wordpress App") 
+		By("Pushing Wordpress App")
 		wordpress := "https://github.com/epinio/example-wordpress,main"
 		pushLog, err := env.EpinioPush("",
 			appName,
@@ -159,7 +159,7 @@ var _ = Describe("Epinio upgrade with running app", func() {
 			"-e", "BP_PHP_SERVER=nginx",
 			"-e", "DB_HOST=x8e5ee833a0f2faebaf5c4171baca-mysql",
 			"-e", "SERVICE_NAME=mycustom-service")
-		time.Sleep(25*time.Second)
+		time.Sleep(25 * time.Second)
 		Expect(err).ToNot(HaveOccurred(), pushLog)
 
 		routeRegexp := regexp.MustCompile(`https:\/\/.*omg.howdoi.website`)
@@ -175,44 +175,44 @@ var _ = Describe("Epinio upgrade with running app", func() {
 				WithRow(appName, WithDate(), "1/1", appName+".*", "", ""),
 			),
 		)
-		
+
 		By("Bind it")
 		out, err = env.Epinio("", "service", "bind", myService, appName)
 		Expect(err).ToNot(HaveOccurred(), out)
-		time.Sleep(10*time.Second)
+		time.Sleep(10 * time.Second)
 
 		By("Verify binding")
-				appShowOut, err := env.Epinio("", "app", "show", appName)
-				Expect(err).ToNot(HaveOccurred())
+		appShowOut, err := env.Epinio("", "app", "show", appName)
+		Expect(err).ToNot(HaveOccurred())
 
-				Expect(appShowOut).To(
-					HaveATable(
-						WithHeaders("KEY", "VALUE"),
-						WithRow("Bound Configurations", "x8e5ee833a0f2faebaf5c4171baca-mysql"),
-					),
-				)		
-		time.Sleep(15*time.Second)
-		
+		Expect(appShowOut).To(
+			HaveATable(
+				WithHeaders("KEY", "VALUE"),
+				WithRow("Bound Configurations", "x8e5ee833a0f2faebaf5c4171baca-mysql"),
+			),
+		)
+		time.Sleep(15 * time.Second)
+
 		// Check that the app is reachable and expected page tile is reached
 		Eventually(func() int {
 			resp, err := env.Curl("GET", route, nil)
 			Expect(err).ToNot(HaveOccurred())
 			defer resp.Body.Close()
 
-			time.Sleep(15*time.Second)
-			
+			time.Sleep(15 * time.Second)
+
 			bodyBytes, err := io.ReadAll(resp.Body)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(resp.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
-			time.Sleep(10*time.Second)		
+			time.Sleep(10 * time.Second)
 			Expect(string(bodyBytes)).To(ContainSubstring("WordPress"))
-			
+
 			return resp.StatusCode
 		}, 30*time.Second, 1*time.Second).Should(Equal(http.StatusOK))
 
 		// Upgrade to current as found in checkout
 		epinioHelper.Upgrade()
-		
+
 		// Note post-upgrade versions of client and server
 		By("Versions after upgrade")
 		env.Versions()
@@ -220,7 +220,7 @@ var _ = Describe("Epinio upgrade with running app", func() {
 		// Restarting app
 		By("Restarting app")
 		out, err = env.Epinio("", "app", "restart", appName)
-		time.Sleep(15*time.Second)
+		time.Sleep(15 * time.Second)
 		Expect(err).ToNot(HaveOccurred(), out)
 
 		// Check that the app is still reachable and expected page tile is reached
@@ -229,17 +229,16 @@ var _ = Describe("Epinio upgrade with running app", func() {
 			Expect(err).ToNot(HaveOccurred())
 			defer resp.Body.Close()
 
-			time.Sleep(15*time.Second)
-			
+			time.Sleep(15 * time.Second)
+
 			bodyBytes, err := io.ReadAll(resp.Body)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(resp.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
-			time.Sleep(10*time.Second)		
+			time.Sleep(10 * time.Second)
 			Expect(string(bodyBytes)).To(ContainSubstring("WordPress"))
-			
+
 			return resp.StatusCode
 		}, 30*time.Second, 1*time.Second).Should(Equal(http.StatusOK))
 	})
-
 
 })
