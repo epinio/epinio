ff967e758fa760d6ff13f10849793c4ddb47d1d8
Enrico Candino <enrico.candino@suse.com>
2023-04-20 12:46:36 +0200

    Added per-release synchronized Helm client (#2226)
    * added per-release synchronized client
    
    * adding debug
    
    * Update main.yml
    
    * added same release in same namespace test, fixed mock instrumentation

--- internal/helm/client_test.go
+++ internal/helm/client_test.go
@@ -27,6 +27,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+type CtxDuration struct{}
+
 var _ = Describe("SynchronizedClient", func() {
 	var (
 		ctx        context.Context
@@ -40,19 +42,19 @@ var _ = Describe("SynchronizedClient", func() {
 		mockClient = hcmock.NewMockClient(mockCtrl)
 	})
 
-	setupMockRelease := func(ctx context.Context, releaseName string, duration time.Duration) *hc.ChartSpec {
-		fakeRelease := &hc.ChartSpec{ReleaseName: releaseName}
-
+	// setupMockRelease will setup the mock. This mock will look into the context for the CtxDuration{} key
+	// to get the time.Duration used to time.Sleep() faking the release.
+	// This was needed to register different InstallOrUpgradeChart() calls (thanks to the difference in the Context)
+	setupMockRelease := func(ctx context.Context, myRelease *hc.ChartSpec) {
 		mockClient.
 			EXPECT().
-			InstallOrUpgradeChart(ctx, fakeRelease, nil).
+			InstallOrUpgradeChart(ctx, myRelease, nil).
 			DoAndReturn(func(ctx context.Context, spec *hc.ChartSpec, opts *hc.GenericHelmOptions) (*release.Release, error) {
+				duration, _ := ctx.Value(CtxDuration{}).(time.Duration)
 				time.Sleep(duration)
 				return &release.Release{Name: spec.ReleaseName}, nil
 			}).
 			AnyTimes()
-
-		return fakeRelease
 	}
 
 	When("getting a namespace synchronized client", func() {
@@ -85,13 +87,15 @@ var _ = Describe("SynchronizedClient", func() {
 
 	When("installing or upgrading chart", func() {
 
-		It("should wait for releases in the same namespace", func() {
-			// setup the mock with a couple of releases
+		It("should wait for the same release in the same namespace", func() {
+			// setup the mock with the same release, twice
 
-			// release2s will take 2s
-			release2s := setupMockRelease(ctx, "release-2s", 2*time.Second)
-			// release3s will take 3s
-			release3s := setupMockRelease(ctx, "release-3s", 3*time.Second)
+			ctx2s := context.WithValue(ctx, CtxDuration{}, 2*time.Second)
+			ctx3s := context.WithValue(ctx, CtxDuration{}, 3*time.Second)
+			myRelease := &hc.ChartSpec{ReleaseName: "my-release"}
+
+			setupMockRelease(ctx2s, myRelease)
+			setupMockRelease(ctx3s, myRelease)
 
 			// create a sync client
 			namespace := catalog.NewNamespaceName()
@@ -103,24 +107,24 @@ var _ = Describe("SynchronizedClient", func() {
 			wg := &sync.WaitGroup{}
 
 			// let's see how long the two installations are taking
-			// since they are done in the same namespace they should take 2s + 3s
+			// since they are done in the same namespace and with the same release they should take 5s
 			start := time.Now()
 
-			// release2s will take 2s
+			// myRelease will take 2s the first time
 			wg.Add(1)
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient.InstallOrUpgradeChart(ctx, release2s, nil)
+				syncClient.InstallOrUpgradeChart(ctx2s, myRelease, nil)
 				wg.Done()
 			}(wg)
 
-			// release3s will take 3s
+			// second installation of myRelease will take 3s
 			wg.Add(1)
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient.InstallOrUpgradeChart(ctx, release3s, nil)
+				syncClient.InstallOrUpgradeChart(ctx3s, myRelease, nil)
 				wg.Done()
 			}(wg)
 
@@ -131,13 +135,70 @@ var _ = Describe("SynchronizedClient", func() {
 			Expect(elapsed).To(BeNumerically(">=", 5*time.Second))
 		})
 
-		It("should not wait for releases in different namespaces and do them concurrently", func() {
+		It("should not wait for different releases in the same namespace", func() {
 			// setup the mock with a couple of releases
 
+			// release2s will take 2s
+			ctx2s := context.WithValue(ctx, CtxDuration{}, 2*time.Second)
+			release2s := &hc.ChartSpec{ReleaseName: "release-2s"}
+			setupMockRelease(ctx2s, release2s)
+
+			// release3s will take 3s
+			ctx3s := context.WithValue(ctx, CtxDuration{}, 3*time.Second)
+			release3s := &hc.ChartSpec{ReleaseName: "release-3s"}
+			setupMockRelease(ctx3s, release3s)
+
+			// create a sync client
+			namespace := catalog.NewNamespaceName()
+			syncClient, err := helm.GetNamespaceSynchronizedHelmClient(namespace, mockClient)
+			Expect(err).To(BeNil())
+
+			// Begin TEST!
+
+			wg := &sync.WaitGroup{}
+
+			// let's see how long the two installations are taking
+			// since they are done in the same namespace but they are different they should take 3s
+			start := time.Now()
+
+			// release2s will take 2s
+			wg.Add(1)
+			go func(wg *sync.WaitGroup) {
+				defer GinkgoRecover()
+
+				syncClient.InstallOrUpgradeChart(ctx2s, release2s, nil)
+				wg.Done()
+			}(wg)
+
+			// release3s will take 3s
+			wg.Add(1)
+			go func(wg *sync.WaitGroup) {
+				defer GinkgoRecover()
+
+				syncClient.InstallOrUpgradeChart(ctx3s, release3s, nil)
+				wg.Done()
+			}(wg)
+
+			wg.Wait()
+
+			// the elapsed time should be greater than 5s
+			elapsed := time.Since(start)
+			Expect(elapsed).To(BeNumerically(">=", 3*time.Second))
+			Expect(elapsed).To(BeNumerically("<", 4*time.Second))
+		})
+
+		It("should not wait for releases in different namespaces and do them concurrently", func() {
+			// setup the mock with a couple of releases with the same 3s duration
+
+			ctx3s := context.WithValue(ctx, CtxDuration{}, 3*time.Second)
+
 			// releaseOne3s will take 3s in the first namespace
-			releaseOne3s := setupMockRelease(ctx, "release-ns1-3s", 3*time.Second)
-			// releaseTwo3s will take 3s in the second namespace
-			releaseTwo3s := setupMockRelease(ctx, "release-ns2-3s", 3*time.Second)
+			releaseOne3s := &hc.ChartSpec{ReleaseName: "release-ns1-3s"}
+			setupMockRelease(ctx3s, releaseOne3s)
+
+			// releaseOne3s will take 3s in the second namespace
+			releaseTwo3s := &hc.ChartSpec{ReleaseName: "release-ns2-3s"}
+			setupMockRelease(ctx3s, releaseTwo3s)
 
 			// create two sync clients
 			namespace1 := catalog.NewNamespaceName()
@@ -161,7 +222,7 @@ var _ = Describe("SynchronizedClient", func() {
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient1.InstallOrUpgradeChart(ctx, releaseOne3s, nil)
+				syncClient1.InstallOrUpgradeChart(ctx3s, releaseOne3s, nil)
 				wg.Done()
 			}(wg)
 
@@ -170,7 +231,7 @@ var _ = Describe("SynchronizedClient", func() {
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient2.InstallOrUpgradeChart(ctx, releaseTwo3s, nil)
+				syncClient2.InstallOrUpgradeChart(ctx3s, releaseTwo3s, nil)
 				wg.Done()
 			}(wg)
 
@@ -186,11 +247,19 @@ var _ = Describe("SynchronizedClient", func() {
 			// setup the mock with three releases
 
 			// release1s will take 1s
-			release1s := setupMockRelease(ctx, "release-1s", 1*time.Second)
+			ctx1s := context.WithValue(ctx, CtxDuration{}, 1*time.Second)
+			release1s := &hc.ChartSpec{ReleaseName: "release-1s"}
+			setupMockRelease(ctx1s, release1s)
+
 			// release3s will take 3s
-			release3s := setupMockRelease(ctx, "release-3s", 3*time.Second)
+			ctx3s := context.WithValue(ctx, CtxDuration{}, 3*time.Second)
+			release3s := &hc.ChartSpec{ReleaseName: "release-3s"}
+			setupMockRelease(ctx3s, release3s)
+
 			// release5s will take 5s
-			release5s := setupMockRelease(ctx, "release-5s", 5*time.Second)
+			ctx5s := context.WithValue(ctx, CtxDuration{}, 5*time.Second)
+			release5s := &hc.ChartSpec{ReleaseName: "release-5s"}
+			setupMockRelease(ctx5s, release5s)
 
 			// create two sync clients
 			namespace1 := catalog.NewNamespaceName()
@@ -214,7 +283,7 @@ var _ = Describe("SynchronizedClient", func() {
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient1.InstallOrUpgradeChart(ctx, release1s, nil)
+				syncClient1.InstallOrUpgradeChart(ctx1s, release1s, nil)
 				wg.Done()
 			}(wg)
 
@@ -223,7 +292,7 @@ var _ = Describe("SynchronizedClient", func() {
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient1.InstallOrUpgradeChart(ctx, release3s, nil)
+				syncClient1.InstallOrUpgradeChart(ctx3s, release3s, nil)
 				wg.Done()
 			}(wg)
 
@@ -232,7 +301,7 @@ var _ = Describe("SynchronizedClient", func() {
 			go func(wg *sync.WaitGroup) {
 				defer GinkgoRecover()
 
-				syncClient2.InstallOrUpgradeChart(ctx, release5s, nil)
+				syncClient2.InstallOrUpgradeChart(ctx5s, release5s, nil)
 				wg.Done()
 			}(wg)
 
