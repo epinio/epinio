31967764b3f96f55eb5b62116a3b326887e52dfe
Andreas Kupries <andreas.kupries@suse.com>
2023-05-19 13:03:33 +0200

    chore: remove unused in kubernetes tailer.

--- helpers/kubernetes/tailer/container_state.go
+++ helpers/kubernetes/tailer/container_state.go
@@ -11,12 +11,6 @@
 
 package tailer
 
-import (
-	"errors"
-
-	v1 "k8s.io/api/core/v1"
-)
-
 type ContainerState string
 
 const (
@@ -24,21 +18,3 @@ const (
 	WAITING    = "waiting"
 	TERMINATED = "terminated"
 )
-
-func NewContainerState(stateConfig string) (ContainerState, error) {
-	if stateConfig == RUNNING {
-		return RUNNING, nil
-	} else if stateConfig == WAITING {
-		return WAITING, nil
-	} else if stateConfig == TERMINATED {
-		return TERMINATED, nil
-	}
-
-	return "", errors.New("containerState should be one of 'running', 'waiting', or 'terminated'")
-}
-
-func (stateConfig ContainerState) Match(containerState v1.ContainerState) bool {
-	return (stateConfig == RUNNING && containerState.Running != nil) ||
-		(stateConfig == WAITING && containerState.Waiting != nil) ||
-		(stateConfig == TERMINATED && containerState.Terminated != nil)
-}
