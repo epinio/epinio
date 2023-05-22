10ff0dd6e1a97d3e5409b78cc3fe3ac40bf72165
Andreas Kupries <andreas.kupries@suse.com>
2023-05-22 14:35:10 +0200

    refactor: move volumes/mounts out of static job setup.

--- internal/api/v1/application/part.go
+++ internal/api/v1/application/part.go
@@ -229,6 +229,45 @@ func runDownloadImageJob(ctx context.Context, cluster *kubernetes.Cluster, jobNa
 		"app.kubernetes.io/component":  "staging",
 	}
 
+	volumes := []corev1.Volume{{
+		Name: "image-export-volume",
+		VolumeSource: corev1.VolumeSource{
+			PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+				ClaimName: "image-export-pvc",
+			},
+		},
+	}, {
+		Name: "registry-cert-volume",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: "epinio-registry-tls",
+			},
+		},
+	}, {
+		Name: "registry-creds-volume",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: registry.CredentialsSecretName,
+				Items: []corev1.KeyToPath{
+					{
+						Key:  ".dockerconfigjson",
+						Path: "auth.json",
+					},
+				},
+			},
+		},
+	}}
+	mounts := []corev1.VolumeMount{{
+		Name:      "image-export-volume",
+		MountPath: "/tmp/",
+	}, {
+		Name:      "registry-cert-volume",
+		MountPath: "/etc/ssl/certs/",
+	}, {
+		Name:      "registry-creds-volume",
+		MountPath: "/root/containers/",
+	}}
+
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        jobName,
@@ -254,47 +293,11 @@ func runDownloadImageJob(ctx context.Context, cluster *kubernetes.Cluster, jobNa
 								"docker://" + imageURL,
 								"docker-archive:/tmp/" + imageOutputFilename,
 							},
-							VolumeMounts: []corev1.VolumeMount{{
-								Name:      "image-export-volume",
-								MountPath: "/tmp/",
-							}, {
-								Name:      "registry-cert-volume",
-								MountPath: "/etc/ssl/certs/",
-							}, {
-								Name:      "registry-creds-volume",
-								MountPath: "/root/containers/",
-							}},
+							VolumeMounts: mounts,
 						},
 					},
 					RestartPolicy: corev1.RestartPolicyNever,
-					Volumes: []corev1.Volume{{
-						Name: "image-export-volume",
-						VolumeSource: corev1.VolumeSource{
-							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-								ClaimName: "image-export-pvc",
-							},
-						},
-					}, {
-						Name: "registry-cert-volume",
-						VolumeSource: corev1.VolumeSource{
-							Secret: &corev1.SecretVolumeSource{
-								SecretName: "epinio-registry-tls",
-							},
-						},
-					}, {
-						Name: "registry-creds-volume",
-						VolumeSource: corev1.VolumeSource{
-							Secret: &corev1.SecretVolumeSource{
-								SecretName: registry.CredentialsSecretName,
-								Items: []corev1.KeyToPath{
-									{
-										Key:  ".dockerconfigjson",
-										Path: "auth.json",
-									},
-								},
-							},
-						},
-					}},
+					Volumes:       volumes,
 				},
 			},
 		},
