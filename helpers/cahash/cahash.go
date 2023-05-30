c5439824dd493c9c058d58c5c98b420801fcf7ac
Enrico Candino <enrico.candino@suse.com>
2023-05-30 11:56:16 +0200

    Added CA certs in validation (#2348)
    * include CA certs in validation
    
    * removed copied functions, added tests
    
    * added deps

--- helpers/cahash/cahash.go
+++ helpers/cahash/cahash.go
@@ -12,16 +12,11 @@
 package cahash
 
 import (
-	"crypto/sha1" // nolint:gosec // Required by subject hash specification
 	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/asn1"
-	"encoding/binary"
 	"encoding/pem"
 	"fmt"
-	"regexp"
-	"strings"
 
+	"github.com/paketo-buildpacks/ca-certificates/cacerts"
 	"github.com/pkg/errors"
 )
 
@@ -33,7 +28,7 @@ func GenerateHash(certRaw []byte) (string, error) {
 		return "", fmt.Errorf("failed to decode certificate\n%w", err)
 	}
 
-	hash, err := SubjectNameHash(cert)
+	hash, err := cacerts.SubjectNameHash(cert)
 	if err != nil {
 		return "", fmt.Errorf("failed compute subject name hash for cert\n%w", err)
 	}
@@ -44,15 +39,15 @@ func GenerateHash(certRaw []byte) (string, error) {
 
 // -----------------------------------------------------------------------------------
 // See gh:paketo-buildpacks/ca-certificates (cacerts/certs.go) for the original code.
-
-// Iterates over pem blocks until a non-CA certificate if found or no other PEM
-// blocks exist.
+// https://github.com/paketo-buildpacks/ca-certificates
+//
+// Iterates over pem blocks until a valid certificate is found or no other PEM blocks exist.
 func DecodeOneCert(raw []byte) (*x509.Certificate, error) {
 	byteData := raw
 	for len(byteData) > 0 {
 		block, rest := pem.Decode(byteData)
 		if block == nil {
-			return nil, errors.New("failed find PEM data")
+			return nil, errors.New("failed decoding PEM data")
 		}
 
 		cert, err := x509.ParseCertificate(block.Bytes)
@@ -60,91 +55,8 @@ func DecodeOneCert(raw []byte) (*x509.Certificate, error) {
 			byteData = rest
 			continue // pem block is not a cert? (e.g. maybe it was a dh_params block)
 		}
-		if !cert.IsCA {
-			return cert, nil
-		}
-		byteData = rest
+		return cert, nil
 	}
 
 	return nil, errors.New("failed find PEM data")
-}
-
-// SubjectNameHash is a reimplementation of the X509_subject_name_hash
-// in openssl. It computes the SHA-1 of the canonical encoding of the
-// certificate's subject name and returns the 32-bit integer
-// represented by the first four bytes of the hash using little-endian
-// byte order.
-func SubjectNameHash(cert *x509.Certificate) (uint32, error) {
-	name, err := CanonicalName(cert.RawSubject)
-	if err != nil {
-		return 0, fmt.Errorf("failed to compute canonical subject name\n%w", err)
-	}
-	hasher := sha1.New() // nolint:gosec // Required by subject hash specification
-	_, err = hasher.Write(name)
-	if err != nil {
-		return 0, fmt.Errorf("failed to compute sha1sum of canonical subject name\n%w", err)
-	}
-	sum := hasher.Sum(nil)
-	return binary.LittleEndian.Uint32(sum[:4]), nil
-}
-
-// canonicalSET holds a of canonicalATVs. Suffix SET ensures it is
-// marshaled as a set rather than a sequence by asn1.Marshal.
-type canonicalSET []canonicalATV
-
-// canonicalATV is similar to pkix.AttributeTypeAndValue but includes
-// tag to ensure all values are marshaled as ASN.1, UTF8String values
-type canonicalATV struct {
-	Type  asn1.ObjectIdentifier
-	Value string `asn1:"utf8"`
-}
-
-// CanonicalName accepts a DER encoded subject name and returns a
-// "Canonical Encoding" matching that returned by the x509_name_canon
-// function in openssl. All string values are transformed with
-// CanonicalString and UTF8 encoded and the leading SEQ header is
-// removed.
-//
-// For more information see
-// https://stackoverflow.com/questions/34095440/hash-algorithm-for-certificate-crl-directory.
-func CanonicalName(name []byte) ([]byte, error) {
-	var origSeq pkix.RDNSequence
-	_, err := asn1.Unmarshal(name, &origSeq)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse subject name\n%w", err)
-	}
-	var result []byte
-	for _, origSet := range origSeq {
-		var canonSet canonicalSET
-		for _, origATV := range origSet {
-			origVal, ok := origATV.Value.(string)
-			if !ok {
-				return nil, errors.New("got unexpected non-string value")
-			}
-			canonSet = append(canonSet, canonicalATV{
-				Type:  origATV.Type,
-				Value: CanonicalString(origVal),
-			})
-		}
-		setBytes, err := asn1.Marshal(canonSet)
-		if err != nil {
-			return nil, fmt.Errorf("failed to marshal canonical name\n%w", err)
-		}
-		result = append(result, setBytes...)
-	}
-	return result, nil
-}
-
-// CanonicalString transforms the given string. All leading and
-// trailing whitespace is trimmed where whitespace is defined as a
-// space, formfeed, tab, newline, carriage return, or vertical tab
-// character. Any remaining sequence of one or more consecutive
-// whitespace characters in replaced with a single ' '.
-//
-// This is a reimplementation of the asn1_string_canon in openssl
-func CanonicalString(s string) string {
-	s = strings.TrimLeft(s, " \f\t\n\v")
-	s = strings.TrimRight(s, " \f\t\n\v")
-	s = strings.ToLower(s)
-	return string(regexp.MustCompile(`[[:space:]]+`).ReplaceAll([]byte(s), []byte(" ")))
 }
--- helpers/cahash/cahash_test.go
+++ helpers/cahash/cahash_test.go
@@ -13,14 +13,119 @@ package cahash_test
 
 import (
 	"github.com/epinio/epinio/helpers/cahash"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("CA Hash CanonicalString", func() {
-	It("properly transforms the string", func() {
-		r := cahash.CanonicalString(" \f\n \v\thello      \t \f \n \v  world\t \v\n \f ")
-		Expect(r).To(Equal("hello world"))
+var _ = Describe("GenerateHash", func() {
+
+	When("cert is from a CA", func() {
+		It("will be decoded ok", func() {
+			hash, err := cahash.GenerateHash([]byte(caCrt))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(hash).ToNot(BeNil())
+			Expect(hash).To(Equal("4a2d0bdb"))
+		})
 	})
+
+	When("cert has CA:TRUE in Key Usage", func() {
+		It("will be decoded ok", func() {
+			hash, err := cahash.GenerateHash([]byte(ca2Crt))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(hash).ToNot(BeNil())
+			Expect(hash).To(Equal("bb7fe9fe"))
+		})
+	})
+
+	When("cert is not a CA certificate", func() {
+		It("will be decoded ok", func() {
+			hash, err := cahash.GenerateHash([]byte(tlsCert))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(hash).ToNot(BeNil())
+			Expect(hash).To(Equal("eea339da"))
+		})
+	})
+
+	When("PEM data does not contain a certificate", func() {
+		It("returns an error", func() {
+			hash, err := cahash.GenerateHash([]byte(dhParams))
+			Expect(err).To(HaveOccurred())
+			Expect(hash).To(Equal(""))
+		})
+	})
+
+	When("byte data are not PEM encoded", func() {
+		It("returns an error", func() {
+			hash, err := cahash.GenerateHash([]byte("jnhsdjknjsdk"))
+			Expect(err).To(HaveOccurred())
+			Expect(hash).To(Equal(""))
+		})
+	})
+
 })
+
+var (
+	caCrt = `-----BEGIN CERTIFICATE-----
+MIIBaDCCAQ6gAwIBAgIQfKPL2KSrus0tgwC+yXWCCjAKBggqhkjOPQQDAjAUMRIw
+EAYDVQQDEwllcGluaW8tY2EwHhcNMjMwNTI5MDk0MDEyWhcNMjMwODI3MDk0MDEy
+WjAUMRIwEAYDVQQDEwllcGluaW8tY2EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC
+AATAGx1D7ouV5MHIUWFwP9yaH4uu65kGP9F9wLden/6Kt5OLOA2R5D8IMSYfg5A2
+M+QsXBNefq/nYylMxwrAglC4o0IwQDAOBgNVHQ8BAf8EBAMCAqQwDwYDVR0TAQH/
+BAUwAwEB/zAdBgNVHQ4EFgQUoAS25Dc5P4gvlo5GJ7gTi0MNrmMwCgYIKoZIzj0E
+AwIDSAAwRQIgLqCFhVfWJb9pRTNRYTpJ0VHi1YIuhbzKKt/PMUN5N4ECIQDhTYcX
+MoTdO/129uqhd26+7dODT6zSF2sBYGS17IvdmA==
+-----END CERTIFICATE-----
+`
+
+	ca2Crt = `-----BEGIN CERTIFICATE-----
+MIIE1zCCA7+gAwIBAgIUSDJy88GCPAKMX961dozA/Fk56ocwDQYJKoZIhvcNAQEL
+BQAwgZoxCzAJBgNVBAYTAkNaMQ4wDAYDVQQIDAVDemVjaDEQMA4GA1UEBwwHUHJh
+Z3VlcjENMAsGA1UECgwEU1VTRTEPMA0GA1UECwwGZXBpbmlvMScwJQYDVQQDDB50
+aC1yZWdpc3RyeS5jaS5lcGluaW8uc3VzZS5kZXYxIDAeBgkqhkiG9w0BCQEWEXRo
+ZWhlamlrQHN1c2UuY29tMB4XDTIzMDUyOTA5Mjc1OVoXDTMzMDUyNjA5Mjc1OVow
+gZoxCzAJBgNVBAYTAkNaMQ4wDAYDVQQIDAVDemVjaDEQMA4GA1UEBwwHUHJhZ3Vl
+cjENMAsGA1UECgwEU1VTRTEPMA0GA1UECwwGZXBpbmlvMScwJQYDVQQDDB50aC1y
+ZWdpc3RyeS5jaS5lcGluaW8uc3VzZS5kZXYxIDAeBgkqhkiG9w0BCQEWEXRoZWhl
+amlrQHN1c2UuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr9Es
+UywTXXGPQ1H57SxIX/JzqgBnwPtqZlRFss7bddGO/oJtIhu806tcn60pVcYtfJa4
+xiz/tCw2YB8LoJNjMO56zSU74t+IJu367jwA0KC88Ym5RjUoZX0QC5xQYQv5dVig
+TgZHyxc3Pp3Gq3+eHX5tEBKqnzHuYM3fHcbzBvtZ/mcBWo4NK+S0y/787Y3GzfmP
+PsUDcSVs/zcWD538JGbjVca/HYXaFon6oy44l3ZcFN6IiUZis1rTDLDMuD7IT+36
+NZu4oGk53xmVQv8sa/JEFtPAqvKLk2dhSBpIym6lKMr+vrLBAaIavjacaAwAkvII
+MSU5SxZ29RhXUfinJQIDAQABo4IBETCCAQ0wgcQGA1UdIwSBvDCBuaGBoKSBnTCB
+mjELMAkGA1UEBhMCQ1oxDjAMBgNVBAgMBUN6ZWNoMRAwDgYDVQQHDAdQcmFndWVy
+MQ0wCwYDVQQKDARTVVNFMQ8wDQYDVQQLDAZlcGluaW8xJzAlBgNVBAMMHnRoLXJl
+Z2lzdHJ5LmNpLmVwaW5pby5zdXNlLmRldjEgMB4GCSqGSIb3DQEJARYRdGhlaGVq
+aWtAc3VzZS5jb22CFEgycvPBgjwCjF/etXaMwPxZOeqHMAwGA1UdEwQFMAMBAf8w
+CwYDVR0PBAQDAgTwMCkGA1UdEQQiMCCCHnRoLXJlZ2lzdHJ5LmNpLmVwaW5pby5z
+dXNlLmRldjANBgkqhkiG9w0BAQsFAAOCAQEAAa4GsrbdvNKsDUTEy2EQIUPlLcJt
+cKnJNByDuvVsObN9/o161OlqIo7pPq02xsrbSWTECtaPbKs4JAw3l8jTu7seQ92p
+bikAGrN4jbMHkLzoltdGQ6wI/qAaEJ18vSAnvHLE0mClC8/UX45oteMO9y0oOPMf
+amYOhy46EVjwhMQDbMGVtqLDTYV4Wh/FOlf0k0SNS/0YwA03EvMlVV6YuCe15NA/
+KsBNWqAwpXLT7hnbs318Tq1MpUnS7SGWS+NQO8lA8FWz8QxEn/hQnXkQZ1xhVJ3Q
+gTx1164+0b5waF86X3PQEVSM4qCNnNBS6CB6kJNGzjE6Ulx+D6HVpOwj0A==
+-----END CERTIFICATE-----`
+
+	tlsCert = `-----BEGIN CERTIFICATE-----
+MIICVjCCAfugAwIBAgIQb+dL7bataJBnRIpe6Y51UjAKBggqhkjOPQQDAjAUMRIw
+EAYDVQQDEwllcGluaW8tY2EwHhcNMjMwNTI5MDk0MDE3WhcNMjMwODI3MDk0MDE3
+WjAAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1M3KAUzBqUVWxggg
+e6XsThNt6gYzlgnMUdHtza/TsBaDEJznI8u8/ppL+tp58IB6UFGHXkJfZHYMhDb8
+xSc4031RevF7W7Y1rKmoFQekN2HWJM2WazWiznar5b+MrYo7CIhFVH4lui1cqBEL
+YIJPPOtEU+E9DROWzSsobmlDfbTSys9suVNTKE9orrU7t6tpvZ3dy6OdQyrxeggF
+Gzr+Xe+sGab7+bEwCG0DjHQ1ap1gV730DJah/mIZr+MzVbysCZH54gM1zgUV/5LA
+qnEvia6h6QG8jjV1QZSyXCqUJXeYnijs/h5R6ck4jfST1CzyNJg2R5KEjpfmpF08
+ruxc5QIDAQABo3gwdjAOBgNVHQ8BAf8EBAMCBaAwDAYDVR0TAQH/BAIwADAfBgNV
+HSMEGDAWgBSgBLbkNzk/iC+WjkYnuBOLQw2uYzA1BgNVHREBAf8EKzApgiFyZWdp
+c3RyeS5lcGluaW8uc3ZjLmNsdXN0ZXIubG9jYWyHBH8AAAEwCgYIKoZIzj0EAwID
+SQAwRgIhAJhtbr9lMKsgwK7L6jOU5VTc065YrDoEj6/xs9UCdhPCAiEAptsuBGMb
+bwsBe8AtriDRezWjmgfs01ScAQkgipp7/HQ=
+-----END CERTIFICATE-----
+`
+
+	dhParams = `-----BEGIN DH PARAMETERS-----
+MIGHAoGBANclzZUHl2R0NYH5D4cIHcfM8ATuk75NeO2iaV3FhcAAfs9ljlOuJaVn
+UDH9qdl9A4YrDi3VPm55r/YHA4v3wx42Xaq4YfbljeGOKfT6HuhIVS9/n3ZjwNFe
+2IAJeiV4VCRAmjVrgZcUodpEK+jEH4tULNS3NO3p6BbvU/6gyCQLAgEC
+-----END DH PARAMETERS-----`
+)
