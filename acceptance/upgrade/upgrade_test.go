e24c2dd5b7dbcbf1c2e401c22a2797a360517870
Manuel Martín <manuel.martin@suse.com>
2023-06-19 18:10:21 +0200

    Adding test tag for focus Putting upgrade test bound into separate file Makeing  bound tests to accept dynamic service naming and other refactoring.

--- acceptance/upgrade/upgrade_test.go
+++ acceptance/upgrade/upgrade_test.go
@@ -12,7 +12,6 @@
 package upgrade_test
 
 import (
-	"io"
 	"net/http"
 	"regexp"
 	"strings"
@@ -27,7 +26,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Epinio upgrade with running app", func() {
+var _ = Describe("<Upgrade1> Epinio upgrade with running app", func() {
 	var (
 		namespace string // Namespace created before upgrade
 		appName   string // Application created before upgrade
@@ -121,124 +120,4 @@ var _ = Describe("Epinio upgrade with running app", func() {
 		// We can think about adding more checks later like application with
 		// environment vars or configurations
 	})
-
-	It("Can upgrade epinio binded to a custom service", func() {
-		// Test variables
-		var myService = "mycustom-service"
-
-		// Note current versions of client and server
-		By("Versions before upgrade")
-		env.Versions()
-
-		// Create a service
-		By("Creating a service")
-		out, err := env.Epinio("", "service", "create", "mysql-dev", myService)
-		Expect(err).ToNot(HaveOccurred(), out)
-
-		By("Wait for deployment")
-		Eventually(func() string {
-			out, _ := env.Epinio("", "service", "show", myService)
-			return out
-		}, "2m", "5s").Should(
-			HaveATable(
-				WithHeaders("KEY", "VALUE"),
-				WithRow("Status", "deployed"),
-			),
-		)
-		time.Sleep(25 * time.Second)
-
-		// Deploy Wordpress application
-		By("Pushing Wordpress App")
-		wordpress := "https://github.com/epinio/example-wordpress,main"
-		pushLog, err := env.EpinioPush("",
-			appName,
-			"--name", appName,
-			"--git", wordpress,
-			"-e", "BP_PHP_WEB_DIR=wordpress",
-			"-e", "BP_PHP_VERSION=8.0.x",
-			"-e", "BP_PHP_SERVER=nginx",
-			"-e", "DB_HOST=x8e5ee833a0f2faebaf5c4171baca-mysql",
-			"-e", "SERVICE_NAME=mycustom-service")
-		time.Sleep(25 * time.Second)
-		Expect(err).ToNot(HaveOccurred(), pushLog)
-
-		routeRegexp := regexp.MustCompile(`https:\/\/.*omg.howdoi.website`)
-		route := string(routeRegexp.Find([]byte(pushLog)))
-
-		Eventually(func() string {
-			out, err := env.Epinio("", "app", "list")
-			Expect(err).ToNot(HaveOccurred(), out)
-			return out
-		}, "5m").Should(
-			HaveATable(
-				WithHeaders("NAME", "CREATED", "STATUS", "ROUTES", "CONFIGURATIONS", "STATUS DETAILS"),
-				WithRow(appName, WithDate(), "1/1", appName+".*", "", ""),
-			),
-		)
-
-		By("Bind it")
-		out, err = env.Epinio("", "service", "bind", myService, appName)
-		Expect(err).ToNot(HaveOccurred(), out)
-		time.Sleep(10 * time.Second)
-
-		By("Verify binding")
-		appShowOut, err := env.Epinio("", "app", "show", appName)
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(appShowOut).To(
-			HaveATable(
-				WithHeaders("KEY", "VALUE"),
-				WithRow("Bound Configurations", "x8e5ee833a0f2faebaf5c4171baca-mysql"),
-			),
-		)
-		time.Sleep(15 * time.Second)
-
-		// Check that the app is reachable and expected page tile is reached
-		Eventually(func() int {
-			resp, err := env.Curl("GET", route, nil)
-			Expect(err).ToNot(HaveOccurred())
-			defer resp.Body.Close()
-
-			time.Sleep(15 * time.Second)
-
-			bodyBytes, err := io.ReadAll(resp.Body)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
-			time.Sleep(10 * time.Second)
-			Expect(string(bodyBytes)).To(ContainSubstring("WordPress"))
-
-			return resp.StatusCode
-		}, 120*time.Second, 10*time.Second).Should(Equal(http.StatusOK))
-
-		// Upgrade to current as found in checkout
-		epinioHelper.Upgrade()
-
-		// Note post-upgrade versions of client and server
-		By("Versions after upgrade")
-		env.Versions()
-
-		// Restarting app
-		By("Restarting app")
-		out, err = env.Epinio("", "app", "restart", appName)
-		time.Sleep(15 * time.Second)
-		Expect(err).ToNot(HaveOccurred(), out)
-
-		// Check that the app is still reachable and expected page tile is reached
-		Eventually(func() int {
-			resp, err := env.Curl("GET", route, nil)
-			Expect(err).ToNot(HaveOccurred())
-			defer resp.Body.Close()
-
-			time.Sleep(15 * time.Second)
-
-			bodyBytes, err := io.ReadAll(resp.Body)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusOK), string(bodyBytes))
-			time.Sleep(10 * time.Second)
-			Expect(string(bodyBytes)).To(ContainSubstring("WordPress"))
-
-			return resp.StatusCode
-		}, 120*time.Second, 10*time.Second).Should(Equal(http.StatusOK))
-	})
-
 })
