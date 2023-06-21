55acb48c0afd9dbb7a7451224ccd0c436ed07c98
Enrico Candino <enrico.candino@suse.com>
2023-06-21 16:43:46 +0200

    added info tests

--- pkg/api/core/v1/client/info.go
+++ pkg/api/core/v1/client/info.go
@@ -12,26 +12,14 @@
 package client
 
 import (
-	"encoding/json"
-
 	api "github.com/epinio/epinio/internal/api/v1"
 	"github.com/epinio/epinio/pkg/api/core/v1/models"
 )
 
 // Info returns information about Epinio and its components
 func (c *Client) Info() (models.InfoResponse, error) {
-	var resp models.InfoResponse
+	response := models.InfoResponse{}
+	endpoint := api.Routes.Path("Info")
 
-	data, err := c.get(api.Routes.Path("Info"))
-	if err != nil {
-		return resp, err
-	}
-
-	if err := json.Unmarshal(data, &resp); err != nil {
-		return resp, err
-	}
-
-	c.log.V(1).Info("response decoded", "response", resp)
-
-	return resp, nil
+	return Get(c, endpoint, response)
 }
