cb6dd51c9ce01b49e8e1a2f3d1b0347e6a9145b5
Andreas Kupries <andreas.kupries@suse.com>
2023-10-11 11:30:09 +0200

    chore: update test to match

--- acceptance/api/v1/info_test.go
+++ acceptance/api/v1/info_test.go
@@ -40,7 +40,7 @@ var _ = Describe("Info endpoint", LMisc, func() {
 		err = json.Unmarshal(bodyBytes, &info)
 		Expect(err).ToNot(HaveOccurred())
 
-		Expect(info.DefaultBuilderImage).To(Equal("paketobuildpacks/builder:full"))
+		Expect(info.DefaultBuilderImage).To(Equal("paketobuildpacks/builder:0.2.443-full"))
 	})
 
 	It("includes the epinio server version in a header", func() {
