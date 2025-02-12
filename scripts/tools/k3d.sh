06dbff1c77dd4248c7184d030c54b591a4137563
Colin Griffin <colin@krum.io>
2025-02-12 16:27:06 -0500

    Update k3d source

--- scripts/tools/k3d.sh
+++ scripts/tools/k3d.sh
@@ -13,8 +13,8 @@ set -e
 
 VERSION="5.6.0"
 
-URL="https://github.com/rancher/k3d/releases/download/v${VERSION}/k3d-linux-amd64"
-SHA256="6744bfd5cea611dc3f2a24da7b25a28fd0dd4b78c486193c238d55619d7b7c43"
+URL="https://github.com/k3d-io/k3d/releases/download/v${VERSION}/k3d-linux-amd64"
+SHA256="c2ca4b90c0c064cd56ae674d586e2003712fe5841748223552e2f8536f741366"
 
 pushd "$TMP_DIR" > /dev/null
 wget -q "$URL" -O k3d
