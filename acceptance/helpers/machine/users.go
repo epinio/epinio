ca94bd28f6123d09a70b743b108bf0e717d674af
David Johnson <djohnson@krum.io>
2025-07-29 20:30:34 -0400

    Update users.go
    fix tabbing

--- acceptance/helpers/machine/users.go
+++ acceptance/helpers/machine/users.go
@@ -35,21 +35,21 @@ func (m *Machine) CreateEpinioUser(role string, namespaces []string) (string, st
 
 	secret := &corev1.Secret{
 		TypeMeta: metav1.TypeMeta{
-			Kind:				"Secret",
+			Kind: "Secret",
 			APIVersion: "v1",
 		},
 		Type: "Opaque",
 		ObjectMeta: metav1.ObjectMeta{
-			Name:			 names.GenerateResourceName("ruser", "test", user),
+			Name: names.GenerateResourceName("ruser", "test", user),
 			Namespace: "epinio",
 			Labels: map[string]string{
 				"epinio.io/api-user-credentials": "true",
-				"epinio.io/role":									role,
+				"epinio.io/role": role,
 			},
 		},
 		StringData: map[string]string{
-			"username":		user,
-			"password":		string(hashedPassword),
+			"username": user,
+			"password": string(hashedPassword),
 			"namespaces": strings.Join(namespaces, "\n"),
 		},
 	}
