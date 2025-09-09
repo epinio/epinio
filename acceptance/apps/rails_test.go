bb6b44e936eb1c771a4295e5dbeb36f2aa400ae3
David Johnson <djohnson@krum.io>
2025-09-09 15:16:23 -0400

    fix typo on mdkir->mkdir

--- acceptance/apps/rails_test.go
+++ acceptance/apps/rails_test.go
@@ -116,7 +116,7 @@ var _ = Describe("RubyOnRails", func() {
 		out, err = proc.RunW("sed", "-i", "-e", fmt.Sprintf("s/myname/%s/", catalogName), testenv.TestAssetPath("my-postgresql-custom-svc.yaml"))
 		Expect(err).ToNot(HaveOccurred(), out)
  		
-		out, err = proc.RunW("mdkir", "-p", "tmp/pids")
+		out, err = proc.RunW("mkdir", "-p", "tmp/pids")
 		Expect(err).ToNot(HaveOccurred(), out)
 		
 		out, err = proc.Kubectl("apply", "-f", testenv.TestAssetPath("my-postgresql-custom-svc.yaml"))
